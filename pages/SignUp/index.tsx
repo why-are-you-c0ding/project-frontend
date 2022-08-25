93bb42a2436ff35e3201a41e4080ba228c724183
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-08-26 00:35:56 +0900

    Feat: 회원가입 페이지 추가

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import {Form, Header, Input, Label, SignUpBtn, Wrapper} from './styles'
 
 const SignUp = () => {
-  return <div>회원가입</div>;
+  return (
+
+      <Wrapper>
+        <Header>회원가입</Header>
+        <Form>
+            <Label>
+                <span>아이디*</span>
+                <button>중복 체크</button>
+                <div>
+                    <Input type="text" value={''}  placeholder="예) Wayc"  />
+                </div>
+            </Label>
+            <Label>
+                <span>비밀 번호*</span>
+                <div>
+                    <Input type="password"  value={''} placeholder="예) 영문, 숫자, 특수문자 조합 8-16자"  />
+                </div>
+            </Label>
+            <Label>
+                <span>비밀 번호 체크*</span>
+                <div>
+                    <Input type="password" value={''}  name="email" placeholder="예) 비밀번호를 정확히 입력해주세요"  />
+                </div>
+            </Label>
+            <Label>
+                <span>이메일 주소*</span>
+                <button>중복 체크</button>
+                <div>
+                    <Input type="email"  value={''}  placeholder="예) Wayc@Wayc.co.kr"  />
+                </div>
+            </Label>
+            <Label>
+                <span>닉네임*</span>
+                <button>중복 체크</button>
+                <div>
+                    <Input type="text" value={''}  placeholder="예) 홍길동"  />
+                </div>
+            </Label>
+            <SignUpBtn>가입하기</SignUpBtn>
+        </Form>
+      </Wrapper>
+  )
 };
 
 export default SignUp;
