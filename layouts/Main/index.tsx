911758c1900b43e51eea8664a0cac99b38747801
5hyun <qhslsl@gmail.com>
2022-08-26 01:44:57 +0900

    Feat: 상단바 기능 추가

--- layouts/Main/index.tsx
+++ layouts/Main/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import StatusBar from "@components/StatusBar";
 
 const Main = () => {
-  return <div>메인</div>;
+  return (
+    <div>
+      <StatusBar />
+    </div>
+  );
 };
 
 export default Main;
