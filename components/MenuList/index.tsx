081a6c73f7ec7bd9856ccd98910821074b494957
5hyun <qhslsl@gmail.com>
2022-08-28 02:27:22 +0900

    Feat: 메뉴 왼쪽 배경 어둡게 변경

--- components/MenuList/index.tsx
+++ components/MenuList/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect } from "react";
-import { CloseBtn, List } from "@components/MenuList/styles";
+import { Blank, CloseBtn, List } from "@components/MenuList/styles";
 
 interface Props {
   onCloseModal: (e: React.SyntheticEvent<EventTarget>) => void;
@@ -7,15 +7,18 @@ interface Props {
 
 const MenuList: FC<Props> = ({ onCloseModal }) => {
   return (
-    <List>
-      <CloseBtn onClick={onCloseModal}>X</CloseBtn>
-      <div>
-        <span>Shop</span>
-        <span>장바구니</span>
-        <span>마이페이지</span>
-        <span>로그인</span>
-      </div>
-    </List>
+    <div>
+      <Blank onClick={onCloseModal}></Blank>
+      <List>
+        <CloseBtn onClick={onCloseModal}>X</CloseBtn>
+        <div>
+          <span>Shop</span>
+          <span>장바구니</span>
+          <span>마이페이지</span>
+          <span>로그인</span>
+        </div>
+      </List>
+    </div>
   );
 };
 
--- components/MenuList/styles.tsx
+++ components/MenuList/styles.tsx
@@ -15,7 +15,7 @@ export const List = styled.div`
   position: fixed;
   right: 0;
 
-  width: 70vw;
+  width: 60vw;
   height: 100%;
 
   background-color: white;
@@ -33,4 +33,13 @@ export const List = styled.div`
       border-bottom: 1px solid rgba(0, 0, 0, 0.2);
     }
   }
+`;
+
+export const Blank = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 40vw;
+  height: 100%;
+  background-color: rgba(0,0,0,0.3);
 `;
