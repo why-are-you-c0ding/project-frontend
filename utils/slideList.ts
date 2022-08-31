e7f43e94fb3380c4009541957056330c3f27f148
5hyun <qhslsl@gmail.com>
2022-08-31 18:53:51 +0900

    Feat: 슬라이드에 사용 할 사진 List

---
+++ utils/slideList.ts
@@ -0,0 +1,17 @@
+export const slideList = [
+  {
+    id: 1,
+    src: "slideImage/img1.jpeg",
+    alt: "첫번째 슬라이드",
+  },
+  {
+    id: 2,
+    src: "slideImage/img2.jpeg",
+    alt: "두번째 슬라이드",
+  },
+  {
+    id: 3,
+    src: "slideImage/img3.jpeg",
+    alt: "세번째 슬라이드",
+  },
+];
