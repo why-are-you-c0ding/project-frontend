05b100a87973c322fbb9a6f4ca2df79a9b06e7e0
5hyun <qhslsl@gmail.com>
2022-09-01 02:50:29 +0900

    Feat: 아주아주 엉성한 슬라이드(수저 필요)

--- layouts/Main/index.tsx
+++ layouts/Main/index.tsx
@@ -1,4 +1,10 @@
-import React, { MutableRefObject, useRef, useState } from "react";
+import React, {
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import StatusBar from "@components/StatusBar";
 import { Inner, Slide, Wrapper } from "@layouts/Main/styles";
 import { slideList } from "@utils/slideList";
@@ -6,12 +12,47 @@ import { slideList } from "@utils/slideList";
 const Main = () => {
   const ref = useRef<any>(null);
 
+  // const first = () => {
+  //   ref.current.style = "translate: 0vw";
+  // };
+  //
+
+  const [size, setSize] = useState(-100);
+
+  useEffect(() => {
+    console.log(size);
+    if (size > 0) {
+      setSize(-((slideList.length - 1) * 100));
+    }
+  }, [size, setSize]);
+
   const nextButton = () => {
-    ref.current.style = "translate: -100vw";
+    // setSize(size - 100);
+    let checkSize = size - 100;
+
+    if (checkSize <= 0) {
+      setSize(checkSize);
+      ref.current.style = `transform: translateX(${size}vw)`;
+    }
   };
 
   const prevButton = () => {
-    ref.current.style = "translate: 100vw";
+    // setSize(size + 100);
+    let checkSize = size + 100;
+
+    if (checkSize <= 0) {
+      setSize(checkSize);
+      ref.current.style = `transform: translateX(${size}vw)`;
+    } else {
+      setSize(-(slideList.length - 1) * 100);
+      ref.current.style = `transform: translateX(${size}vw)`;
+    }
+  };
+
+  const slideBtn = (v: number): void => {
+    let width = 100 * (v-1);
+    setSize(-width);
+    ref.current.style = `transform: translateX(-${width}vw)`;
   };
 
   return (
@@ -27,11 +68,20 @@ const Main = () => {
             );
           })}
         </Slide>
+      </Wrapper>
+      <div>
         <div>
           <button onClick={prevButton}>prev</button>
           <button onClick={nextButton}>next</button>
         </div>
-      </Wrapper>
+        {slideList.map((v) => {
+          return (
+            <button key={v.id} onClick={() => slideBtn(v.id)}>
+              {v.id}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- layouts/Main/styles.tsx
+++ layouts/Main/styles.tsx
@@ -1,21 +1,26 @@
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
 export const Wrapper = styled.div`
   margin-top: 8rem;
 
   overflow: hidden;
+
+  height: 50vh;
+`;
+
+export const Slide = styled.div`
+  width: 400vw;
+
+  transition: transform 0.5s;
 `;
 
 export const Inner = styled.div`
   width: 100vw;
   float: left;
-`;
-
-export const Slide = styled.div`
-  display: flex;
-  width: 300vw;
 
   & img {
     width: 100%;
+    height: 100%;
   }
 `;
