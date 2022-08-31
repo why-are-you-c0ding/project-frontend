d6acfad040ee223e8d5a9cf6e5f2fdcc8059c3fc
5hyun <qhslsl@gmail.com>
2022-08-31 18:53:14 +0900

    Feat: 슬라이드 구현 중(아직 미완성)

--- layouts/Main/index.tsx
+++ layouts/Main/index.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { MutableRefObject, useRef, useState } from "react";
 import StatusBar from "@components/StatusBar";
+import { Inner, Slide, Wrapper } from "@layouts/Main/styles";
+import { slideList } from "@utils/slideList";
 
 const Main = () => {
+  const ref = useRef<any>(null);
+
+  const nextButton = () => {
+    ref.current.style = "translate: -100vw";
+  };
+
+  const prevButton = () => {
+    ref.current.style = "translate: 100vw";
+  };
+
   return (
     <div>
       <StatusBar />
+      <Wrapper>
+        <Slide ref={ref}>
+          {slideList.map((v) => {
+            return (
+              <Inner key={v.id}>
+                <img src={v.src} alt={v.alt} />
+              </Inner>
+            );
+          })}
+        </Slide>
+        <div>
+          <button onClick={prevButton}>prev</button>
+          <button onClick={nextButton}>next</button>
+        </div>
+      </Wrapper>
     </div>
   );
 };
---
+++ layouts/Main/styles.tsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  margin-top: 8rem;
+
+  overflow: hidden;
+`;
+
+export const Inner = styled.div`
+  width: 100vw;
+  float: left;
+`;
+
+export const Slide = styled.div`
+  display: flex;
+  width: 300vw;
+
+  & img {
+    width: 100%;
+  }
+`;
