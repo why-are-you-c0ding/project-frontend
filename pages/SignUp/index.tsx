4296ec3453433c41313f57a9036f415f43bf4739
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-09-01 01:08:13 +0900

    Chore: 오타 수정

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import useInput from "@hooks/useInput";
 import { Link } from "react-router-dom";
-// import axios from "axios";
+import axios from "axios";
 
 import {
   Form,
@@ -25,6 +25,8 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [nickname, onChangeNickname] = useInput("");
   const [birthDay, onChangeBirthDay, setBirthDay] = useInput("");
+  const [signUpError, setSignUpError] = useState('');
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
 
   const [mismatchError, setMismatchError] = useState(false);
   const [match, setmatch] = useState(true);
@@ -42,21 +44,21 @@ const SignUp = () => {
   }, [birthDay, onChangeBirthDay, setBirthDay]);
 
   const onChangePassword = useCallback(
-    (e) => {
-      setPassword(e.target.value);
-      setMismatchError(e.target.value !== passwordCheck);
-      setmatch(e.target.value === passwordCheck);
-    },
-    [passwordCheck]
+      (e) => {
+        setPassword(e.target.value);
+        setMismatchError(e.target.value !== passwordCheck);
+        setmatch(e.target.value === passwordCheck);
+      },
+      [passwordCheck]
   );
 
   const onChangePasswordCheck = useCallback(
-    (e) => {
-      setPasswordCheck(e.target.value);
-      setMismatchError(e.target.value !== password);
-      setmatch(e.target.value === password);
-    },
-    [password]
+      (e) => {
+        setPasswordCheck(e.target.value);
+        setMismatchError(e.target.value !== password);
+        setmatch(e.target.value === password);
+      },
+      [password]
   );
 
   const onChangeEmail = useCallback((e) => {
@@ -80,155 +82,157 @@ const SignUp = () => {
   //백엔드에 있는 아이디와 현재 아이디 비교 후 오류 출력
 
   const onSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      // axios.post('',{
-      //   id,password,birthDay,email, nickname,
-      // })
-      //     .then((response)=>{
-      //       alert("성공");
-      //       console.log(response);
-      //       setSignUpSuccess(true);
-      //     })
-      //     .catch((error)=>{
-      //       alert("에러!!!!!!");
-      //       console.log(error.response);
-      //       setSignUpError(error.response.data);
-      //
-      //     })
-      //     .finally(()=>{});
-    },
-    [id, password, passwordCheck, birthDay, email, nickname]
+      (e) => {
+        e.preventDefault();
+        axios.post('https://waycabvav.shop/members',{
+          id,password,birthDay,email, nickname,
+        })
+            .then((response)=>{
+              alert("성공");
+              console.log(response);
+              setSignUpSuccess(true);
+            })
+            .catch((error)=>{
+              alert("에러!!!!!!");
+              console.log(error.response);
+              setSignUpError(error.response.data);
+
+            })
+            .finally(()=>{});
+      },
+      [id, password, passwordCheck, birthDay, email, nickname]
   );
 
   return (
-    <div>
-      <StatusBar />
-      <Wrapper>
-        <Header>회원가입</Header>
-        <Form onSubmit={onSubmit}>
-          <Label>
-            <div>
-              <span>아이디*</span>
-              <button onClick={onClickid}>중복 체크</button>
-              {idmissmatch && (
+      <div>
+        <StatusBar />
+        <Wrapper>
+          <Header>회원가입</Header>
+          <Form onSubmit={onSubmit}>
+            <Label>
+              <div>
+                <span>아이디*</span>
+                <button onClick={onClickid}>중복 체크</button>
+                {idmissmatch && (
+                    <Correct>
+                      {" "}
+                      사용 가능합니다
+                    </Correct>
+                )}
+              </div>
+              <Input
+                  type="text"
+                  id="id"
+                  name="id"
+                  value={id}
+                  onChange={onChangeId}
+                  placeholder="예) Wayc"
+              />
+            </Label>
+            <Label>
+              <span>비밀 번호*</span>
+              <div>
+                <Input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={onChangePassword}
+                    placeholder="예) 영문, 숫자, 특수문자 조합 8-16자"
+                />
+              </div>
+            </Label>
+            <Label>
+              <span>비밀 번호 체크* </span>
+              {mismatchError && (
+                  <Error>
+                    {" "}
+                    비밀번호가 일치하지 않습니다!
+                    <ErrorCircle></ErrorCircle>
+                  </Error>
+              )}
+              {match && (
                   <Correct>
                     {" "}
-                    사용 가능합니다
+                    비밀번호가 일치합니다!
+                    <CorrectCircle></CorrectCircle>
                   </Correct>
               )}
-            </div>
-            <Input
-              type="text"
-              id="id"
-              name="id"
-              value={id}
-              onChange={onChangeId}
-              placeholder="예) Wayc"
-            />
-          </Label>
-          <Label>
-            <span>비밀 번호*</span>
-            <div>
-              <Input
-                type="password"
-                id="password"
-                name="password"
-                value={password}
-                onChange={onChangePassword}
-                placeholder="예) 영문, 숫자, 특수문자 조합 8-16자"
-              />
-            </div>
-          </Label>
-          <Label>
-            <span>비밀 번호 체크* </span>
-            {mismatchError && (
-              <Error>
-                {" "}
-                비밀번호가 일치하지 않습니다!
-                <ErrorCircle></ErrorCircle>
-              </Error>
-            )}
-            {match && (
-              <Correct>
-                {" "}
-                비밀번호가 일치합니다!
-                <CorrectCircle></CorrectCircle>
-              </Correct>
-            )}
 
-            <div>
+              <div>
+                <Input
+                    type="password"
+                    id="password-check"
+                    name="password-check"
+                    value={passwordCheck}
+                    onChange={onChangePasswordCheck}
+                    placeholder="예) 영문, 숫자, 특수문자 조합 8-16자"
+                />
+              </div>
+            </Label>
+            <Label>
+              <span>생년 월일*</span>
               <Input
-                type="password"
-                id="password-check"
-                name="password-check"
-                value={passwordCheck}
-                onChange={onChangePasswordCheck}
-                placeholder="예) 영문, 숫자, 특수문자 조합 8-16자"
-              />
-            </div>
-          </Label>
-          <Label>
-            <span>생년 월일*</span>
-            <Input
-              type="date"
-              value={birthDay}
-              onChange={onChangeBirthDay}
-            ></Input>
-          </Label>
-          <Label>
-            <div>
-              <span>이메일 주소*</span>
-              <button onClick={onClickemail}>중복 체크</button>
-              {emailmissmatch && (
-                  <Correct>
-                    {" "}
-                    사용 가능합니다
-                  </Correct>
-              )}
-            </div>
-            <div>
-              <Input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={onChangeEmail}
-                placeholder="예) wayc@google.com"
-              />
-            </div>
-          </Label>
+                  type="date"
+                  value={birthDay}
+                  onChange={onChangeBirthDay}
+              ></Input>
+            </Label>
+            <Label>
+              <div>
+                <span>이메일 주소*</span>
+                <button onClick={onClickemail}>중복 체크</button>
+                {emailmissmatch && (
+                    <Correct>
+                      {" "}
+                      사용 가능합니다
+                    </Correct>
+                )}
+              </div>
+              <div>
+                <Input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={onChangeEmail}
+                    placeholder="예) wayc@google.com"
+                />
+              </div>
+            </Label>
 
-          <Label>
-            <div>
-              <span>닉네임*</span>
-              <button onClick={onClicknickname}>중복 체크</button>
-              {nicknamemissmatch && (
-                  <Correct>
-                    {" "}
-                    사용 가능합니다
-                  </Correct>
-              )}
-            </div>
-            <div>
-              <Input
-                type="text"
-                id="nickname"
-                name="nickname"
-                value={nickname}
-                onChange={onChangeNickname}
-                placeholder="예) 권오현 진짜 못생김"
-              />
-            </div>
-          </Label>
-          <SignUpBtn type="submit">가입하기</SignUpBtn>
-        </Form>
-        <LinkContainer>
-          이미 회원이신가요? &nbsp;
-          <Link to="/login">로그인 하러가기</Link>
-        </LinkContainer>
-      </Wrapper>
-    </div>
+            <Label>
+              <div>
+                <span>닉네임*</span>
+                <button onClick={onClicknickname}>중복 체크</button>
+                {nicknamemissmatch && (
+                    <Correct>
+                      {" "}
+                      사용 가능합니다
+                    </Correct>
+                )}
+              </div>
+              <div>
+                <Input
+                    type="text"
+                    id="nickname"
+                    name="nickname"
+                    value={nickname}
+                    onChange={onChangeNickname}
+                    placeholder="예) 권오현 진짜 못생김"
+                />
+              </div>
+            </Label>
+            {signUpError && <Error>{signUpError}</Error>}
+            {signUpSuccess && <Correct>회원가입되었습니다! 로그인해주세요.</Correct>}
+            <SignUpBtn type="submit">가입하기</SignUpBtn>
+          </Form>
+          <LinkContainer>
+            이미 회원이신가요? &nbsp;
+            <Link to="/login">로그인 하러가기</Link>
+          </LinkContainer>
+        </Wrapper>
+      </div>
   );
 };
 
