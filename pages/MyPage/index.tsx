437514e397d4aa8c123acfddfaf64d9780607922
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-09-01 23:05:17 +0900

    Docs: 새로운 페이지 추가

---
+++ pages/MyPage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const MyPage =()=>{
+    return (
+        <div>마이페이지</div>
+    )
+};
+export default MyPage;
---
+++ pages/Shop/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import StatusBar from "@components/StatusBar";
+import { Wrapper } from "./styles";
+
+const Shop =()=>{
+    return (
+        <div>
+            <StatusBar />
+            <Wrapper>
+                shop
+            </Wrapper>
+        </div>
+    )
+};
+export default Shop;
