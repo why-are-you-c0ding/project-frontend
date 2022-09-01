da68ba669813ee8777e33d12a62f259e83560400
5hyun <qhslsl@gmail.com>
2022-09-02 03:25:34 +0900

    Chore: axios 주소 수정

--- client.tsx
+++ client.tsx
@@ -3,13 +3,13 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import axios from 'axios';
+import axios from 'axios';
 
 import App from '@layouts/App/App';
 
-// axios.defaults.withCredentials = true;
-// axios.defaults.baseURL =
-//   process.env.NODE_ENV === 'production' ? 'https://sleact.nodebird.com' : 'http://localhost:3090';
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'production' ? 'https://sleact.nodebird.com' : 'https://waycabvav.shop';
 
 render(
   <BrowserRouter>
