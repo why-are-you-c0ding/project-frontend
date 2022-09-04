4351d44b1fbd6ceed76a217b1b083c33896b7f0d
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-09-04 23:07:09 +0900

    Feat: 로그인 정보 저장 swr 구현(미완성)

--- pages/LogIn/index.tsx
+++ pages/LogIn/index.tsx
@@ -1,30 +1,55 @@
 import React, {useCallback, useState} from "react";
-import {Form, Header, Input, Label, Line, SearchBox, SignUpBtn, SubHeader, Wrapper} from '@pages/SignUp/styles'
+import {
+  Form,
+  Header,
+  Input,
+  Label,
+  SearchBox,
+  SignUpBtn,
+  SubHeader,
+  Wrapper,
+  Error,
+} from '@pages/SignUp/styles'
 import StatusBar from "@components/StatusBar";
 import useInput from "@hooks/useInput";
 import { Link } from 'react-router-dom';
+import axios from "axios";
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+
+
 
 
 
 const LogIn = () => {
+  const { data, error, mutate } = useSWR('https://waycabvav.shop/', fetcher);
+  //요기 주소도 수빈이(로그인 정보 받아올곳)
   const [id, onChangeId] = useInput("");
-  const [password, setPassword] = useState("");
+  const [password, onChangePassword] = useInput('');
+  const [logInError, setLogInError] = useState(false);
 
-
-  const onChangePassword = useCallback(
-      (e) => {
-        setPassword(e.target.value);
-      },
-      []
-  );
 
   const onSubmit = useCallback(
       (e) => {
         e.preventDefault();
-        console.log(id, password);
+        setLogInError(false);
+        axios
+            .post('https://waycabvav.shop/',
+                { id, password },
+                { withCredentials: true })
+            .then((response) => {
+              alert("성공");
+              mutate(response.data, false);
+            })
+            .catch((error) => {
+              alert("에러");
+              setLogInError(error.response?.data?.statuseCode === 401);
+            });
       },
-      [id, password, ]
+      [id, password],
   );
+  // 로그인 주소 물어봐야됨
+
 
   return (
   <Wrapper>
@@ -57,6 +82,7 @@ const LogIn = () => {
               placeholder="예) 영문, 숫자, 특수문자 조합 8-16자"
           />
         </div>
+          {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
       </Label>
       <SignUpBtn>Log In</SignUpBtn>
         <SearchBox>
@@ -66,7 +92,6 @@ const LogIn = () => {
           <span>아이디 찾기</span>
           <span>비밀번호 찾기</span>
         </SearchBox>
-
     </Form>
   </Wrapper>
   )
