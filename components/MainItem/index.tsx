8b4370648e4f60df4ee690b36df648db4e579146
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-09-06 21:38:22 +0900

    Style: 상품페이지 전체 스타일 조정

--- components/MainItem/index.tsx
+++ components/MainItem/index.tsx
@@ -1,7 +1,7 @@
 import React, {} from "react";
 
-import {SubTitle, Title, TitleBox, Wrapper,ItemBox, ItemList,ItemImg, ItemSub, ItemName, ItemFullName, ItemPrice,RightPrice} from "@components/MainItem/styles";
-import { Link } from "react-router-dom";
+import {Wrapper,TitleContainer, TitleBox, Title, SubTitle,ItemContainer,ItemBox,Item,Itemdetail,ItemName,ItemFullName,ItemPrice,RightPrice} from "@components/MainItem/styles";
+
 
 
 
@@ -9,56 +9,62 @@ import { Link } from "react-router-dom";
 const MainItem =()=>{
     return (
         <Wrapper>
-            <TitleBox>
-                <Title>인기 상품</Title>
-                <SubTitle>발매 상품</SubTitle>
-            </TitleBox>
-            <ItemBox>
-                <ItemList >
-                    <ItemImg>
-                        <img src='https://petnolza.com/wp-content/uploads/2021/11/dog-smart7.jpg'/>
-                    </ItemImg>
-                    <ItemSub>
-                        <ItemName>리트리버</ItemName>
-                        <ItemFullName>레드라도 리트리버</ItemFullName>
-                        <ItemPrice>2억</ItemPrice>
-                        <RightPrice>즉시구매가</RightPrice>
-                    </ItemSub>
-                </ItemList>
-                <ItemList >
-                    <ItemImg>
-                        <img src='https://petnolza.com/wp-content/uploads/2021/11/dog-smart7.jpg'/>
-                    </ItemImg>
-                    <ItemSub>
-                        <ItemName>리트리버</ItemName>
-                        <ItemFullName>레드라도 리트리버</ItemFullName>
-                        <ItemPrice>2억</ItemPrice>
-                        <RightPrice>즉시구매가</RightPrice>
-                    </ItemSub>
-                </ItemList>
-                <ItemList >
-                    <ItemImg>
-                        <img src='https://petnolza.com/wp-content/uploads/2021/11/dog-smart7.jpg'/>
-                    </ItemImg>
-                    <ItemSub>
-                        <ItemName>리트리버</ItemName>
-                        <ItemFullName>레드라도 리트리버</ItemFullName>
-                        <ItemPrice>2억</ItemPrice>
-                        <RightPrice>즉시구매가</RightPrice>
-                    </ItemSub>
-                </ItemList>
-                <ItemList >
-                    <ItemImg>
-                        <img src='https://petnolza.com/wp-content/uploads/2021/11/dog-smart7.jpg'/>
-                    </ItemImg>
-                    <ItemSub>
-                        <ItemName>리트리버</ItemName>
-                        <ItemFullName>레드라도 리트리버</ItemFullName>
-                        <ItemPrice>2억</ItemPrice>
-                        <RightPrice>즉시구매가</RightPrice>
-                    </ItemSub>
-                </ItemList>
-            </ItemBox>
+            <TitleContainer>
+                <TitleBox>
+                    <Title>
+                        Just Dropped
+                    </Title>
+                    <SubTitle>
+                        발매 상품
+                    </SubTitle>
+                </TitleBox>
+            </TitleContainer>
+            <ItemContainer>
+                <ItemBox>
+                    <Item>
+                        <img src="https://post-phinf.pstatic.net/MjAyMTAzMTNfODUg/MDAxNjE1NjAxNjIwOTQz.81Ruw-E3aK-7V89c5JacxvnbWFe8ez_6dGT3S3AC-g8g.ZcQgsuG13-tR16wrj6B32vOIi3axULarz9eoWwBjo2kg.PNG/%EC%B9%98%EC%BD%94%EB%A6%AC%ED%83%80_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-downDFG.png?type=w1200" alt="" />
+                    </Item>
+                    <Itemdetail>
+                        <ItemName>제발</ItemName>
+                        <ItemFullName>제발제발</ItemFullName>
+                        <ItemPrice>제발제발</ItemPrice>
+                        <RightPrice>제발제발</RightPrice>
+                    </Itemdetail>
+                </ItemBox>
+                <ItemBox>
+                    <Item>
+                        <img src="https://post-phinf.pstatic.net/MjAyMTAzMTNfODUg/MDAxNjE1NjAxNjIwOTQz.81Ruw-E3aK-7V89c5JacxvnbWFe8ez_6dGT3S3AC-g8g.ZcQgsuG13-tR16wrj6B32vOIi3axULarz9eoWwBjo2kg.PNG/%EC%B9%98%EC%BD%94%EB%A6%AC%ED%83%80_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-downDFG.png?type=w1200" alt="" />
+                    </Item>
+                    <Itemdetail>
+                        <ItemName>제발</ItemName>
+                        <ItemFullName>제발제발</ItemFullName>
+                        <ItemPrice>제발제발</ItemPrice>
+                        <RightPrice>제발제발</RightPrice>
+                    </Itemdetail>
+                </ItemBox>
+                <ItemBox>
+                    <Item>
+                        <img src="https://post-phinf.pstatic.net/MjAyMTAzMTNfODUg/MDAxNjE1NjAxNjIwOTQz.81Ruw-E3aK-7V89c5JacxvnbWFe8ez_6dGT3S3AC-g8g.ZcQgsuG13-tR16wrj6B32vOIi3axULarz9eoWwBjo2kg.PNG/%EC%B9%98%EC%BD%94%EB%A6%AC%ED%83%80_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-downDFG.png?type=w1200" alt="" />
+                    </Item>
+                    <Itemdetail>
+                        <ItemName>제발</ItemName>
+                        <ItemFullName>제발제발</ItemFullName>
+                        <ItemPrice>제발제발</ItemPrice>
+                        <RightPrice>제발제발</RightPrice>
+                    </Itemdetail>
+                </ItemBox>
+                <ItemBox>
+                    <Item>
+                        <img src="https://post-phinf.pstatic.net/MjAyMTAzMTNfODUg/MDAxNjE1NjAxNjIwOTQz.81Ruw-E3aK-7V89c5JacxvnbWFe8ez_6dGT3S3AC-g8g.ZcQgsuG13-tR16wrj6B32vOIi3axULarz9eoWwBjo2kg.PNG/%EC%B9%98%EC%BD%94%EB%A6%AC%ED%83%80_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-downDFG.png?type=w1200" alt="" />
+                    </Item>
+                    <Itemdetail>
+                        <ItemName>제발</ItemName>
+                        <ItemFullName>제발제발</ItemFullName>
+                        <ItemPrice>제발제발</ItemPrice>
+                        <RightPrice>제발제발</RightPrice>
+                    </Itemdetail>
+                </ItemBox>
+            </ItemContainer>
         </Wrapper>
     )
 };
--- components/MainItem/styles.tsx
+++ components/MainItem/styles.tsx
@@ -1,76 +1,81 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.section`
-  max-width: 80%;
-  display: block;
-  margin: 5rem auto 5rem;
-`;
+`
 
-export const TitleBox = styled.div`
-  padding: 1rem;
-  max-width: 90vh;
-`;
+export const TitleContainer = styled.section`
+    margin: 2.5rem 0 auto;
+    max-width: 80rem;
+    padding: 0 2.5rem;
+    height:2.5rem;
+`
+
+export const TitleBox = styled.section`
+  margin:0 auto;
+  padding:0;
+`
+
 
 export const Title = styled.div`
-  font-size: 1.5rem;
-  font-weight: bolder;
-  margin-bottom: 0.3rem;
-`;
+  font-size:1.5rem;
+  line-height: 1.5rem;
+  margin:0;
+  padding:0;
+`
 
 export const SubTitle = styled.div`
-  color: gray;
   font-size: 0.8rem;
-`;
+  line-height: 1rem;
+  color:gray;
+  font-weight: lighter;
+  margin:0;
+  padding:0;
+`
+
+export const ItemContainer = styled.div`
+  max-width: 100rem;
+  padding:1.8rem 1.8rem;
+  margin:0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: repeat(1,minmax(auto,auto));
+  grid-gap:1rem;
+`
 
 export const ItemBox = styled.div`
   width: 100%;
-  display: grid;
-  grid-template-rows: 1fr;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  align-items: center;
-`;
-
-export const ItemList = styled.div`
-  width: 100%;
   height: 28rem;
-`;
+`
 
-
-export const ItemImg = styled.div`
+export const Item =styled.div`
   width: 18rem;
   height: 18rem;
   border-radius: 1rem;
   margin: auto;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  & img{
-    width: 18rem;
-    height: 18rem;
-  }
-`;
-
-export const ItemSub = styled.div`
+ `
+export const Itemdetail= styled.div`
   padding: 1rem;
-`;
+`
+
 
 export const ItemName = styled.div`
   font-weight: bold;
   font-size: 0.9rem;
   text-decoration: underline;
-  margin-bottom: 1rem;
+  margin-bottom: 0.5rem;
 `;
 
 export const ItemFullName = styled.div`
-  font-size: 1rem;
+  font-size: 0.8rem;
   margin-bottom: 0.5rem;
 `;
 
 export const ItemPrice = styled.div`
-  font-weight: bolder;
+  font-weight: bold;
 `;
 
 export const RightPrice = styled.div`
-  font-size: 0.8rem;
+  font-size: 0.75rem;
   color: gray;
 `;
