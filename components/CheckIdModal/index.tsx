be06f32fd48cfcc2aadef88e76b6f32839e35bc4
5hyun <qhslsl@gmail.com>
2022-09-06 09:39:33 +0900

    Feat: id 중복체크 모달 파일 생성

---
+++ components/CheckIdModal/index.tsx
@@ -0,0 +1,10 @@
+// import React, { FC } from "react";
+// // import { Wrapper } from "@components/CheckIdModal/styles";
+//
+// interface Props {}
+//
+// const CheckIdModal: FC<Props> = () => {
+//   return <div>1</div>;
+// };
+//
+// export default CheckIdModal;
---
+++ components/CheckIdModal/styles.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  position: absolute;
+  top: 500px;
+  left: 500px;
+`;
