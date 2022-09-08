3a88534ea0170ba1ec70e6ac55b3c202b031df5e
5hyun <qhslsl@gmail.com>
2022-09-09 02:21:01 +0900

    Style: 마이페이지 구매 내역까지 스타일링

--- pages/MyPage/index.tsx
+++ pages/MyPage/index.tsx
@@ -1,27 +1,63 @@
-import React from 'react';
+import React from "react";
 import StatusBar from "@components/StatusBar";
-import {Wrapper,UserProfile,SideBar,RightSide} from "@pages/MyPage/styles";
+import {
+  Wrapper,
+  UserProfile,
+  SideBar,
+  RightSide,
+  List,
+  LoginId,
+  Email,
+  EditBtn,
+  BuyItem,
+  BuyStatus,
+  Letter,
+  Count,
+} from "@pages/MyPage/styles";
 
-
-const MyPage =()=>{
-    return (
-        <Wrapper>
-            <StatusBar/>
-            <SideBar>
-
-            </SideBar>
-            <RightSide>
-                <UserProfile>
-                    <img src="https://post-phinf.pstatic.net/MjAyMTAzMTNfODUg/MDAxNjE1NjAxNjIwOTQz.81Ruw-E3aK-7V89c5JacxvnbWFe8ez_6dGT3S3AC-g8g.ZcQgsuG13-tR16wrj6B32vOIi3axULarz9eoWwBjo2kg.PNG/%EC%B9%98%EC%BD%94%EB%A6%AC%ED%83%80_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-downDFG.png?type=w1200" alt=""/>
-                    <div>
-                        <div>권오현</div>
-                        <div>진짜 못생김</div>
-                    </div>
-                </UserProfile>
-            </RightSide>
-
-
-        </Wrapper>
-    )
+const MyPage = () => {
+  return (
+    <div>
+      <StatusBar />
+      <Wrapper>
+        <SideBar>여긴 사이드 바</SideBar>
+        <RightSide>
+          <UserProfile>
+            {/* 이미지 그라바타로 구현 */}
+            <img
+              src="https://post-phinf.pstatic.net/MjAyMTAzMTNfODUg/MDAxNjE1NjAxNjIwOTQz.81Ruw-E3aK-7V89c5JacxvnbWFe8ez_6dGT3S3AC-g8g.ZcQgsuG13-tR16wrj6B32vOIi3axULarz9eoWwBjo2kg.PNG/%EC%B9%98%EC%BD%94%EB%A6%AC%ED%83%80_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-downDFG.png?type=w1200"
+              alt=""
+            />
+            <List>
+              <LoginId>chuseonggun</LoginId>
+              <Email>c+@linux.com</Email>
+              <EditBtn>프로필 수정</EditBtn>
+            </List>
+          </UserProfile>
+          <BuyItem>
+            <span>구매 내역</span>
+            <BuyStatus>
+              <div>
+                <Letter>전체</Letter>
+                <Count>18</Count>
+              </div>
+              <div>
+                <Letter>입금/결제</Letter>
+                <Count>0</Count>
+              </div>
+              <div>
+                <Letter>구매확정</Letter>
+                <Count>15</Count>
+              </div>
+              <div>
+                <Letter>배송완료</Letter>
+                <Count>3</Count>
+              </div>
+            </BuyStatus>
+          </BuyItem>
+        </RightSide>
+      </Wrapper>
+    </div>
+  );
 };
 export default MyPage;
--- pages/MyPage/styles.tsx
+++ pages/MyPage/styles.tsx
@@ -1,29 +1,96 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 0.3fr 1fr;
 `;
 
 export const SideBar = styled.section`
-    font-weight: bolder;
+  font-weight: bolder;
+  border: 1px solid;
 `;
 
 export const RightSide = styled.section`
-  min-height: 20rem;
-  width: 100%;
-`
+  border: 1px solid;
+  padding: 0 4rem;
+`;
 
 export const UserProfile = styled.section`
   display: flex;
-  gap:2rem;
+
+  gap: 1rem;
   border-radius: 20px;
-  background-color: #d6abab;
-  padding: 3rem 3rem;
-  border: 2px solid #d6abab;
-  & img{
+  padding: 2rem 2rem;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+
+  & img {
     width: 7rem;
     height: 7rem;
-    border: 1px solid black;
+    border: 1px solid rgba(0, 0, 0, 0.3);
     border-radius: 50%;
   }
-`
+`;
 
+export const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const LoginId = styled.span`
+  font-size: 1.4rem;
+  font-weight: 600;
+`;
+
+export const Email = styled.span`
+  margin-bottom: 0.8rem;
+  font-size: 0.9rem;
+
+  color: rgba(0, 0, 0, 0.4);
+`;
+
+export const EditBtn = styled.button`
+  padding: 1rem 1.3rem;
+
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  background-color: inherit;
+  color: rgba(0, 0, 0, 0.4);
+  border-radius: 20px;
+`;
+
+export const BuyItem = styled.div`
+  margin-top: 2rem;
+
+  & > span {
+    font-size: 1.3rem;
+  }
+`;
+
+export const BuyStatus = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+
+  margin-top: 1rem;
+  height: 15vh;
+
+  border: 1px solid;
+  border-radius: 10px;
+
+  & div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export const Letter = styled.span`
+  color: rgba(0, 0, 0, 0.5);
+  font-size: 1.1rem;
+`;
+
+export const Count = styled.span`
+  font-size: 1.1rem;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+`;
