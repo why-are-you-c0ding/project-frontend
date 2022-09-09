123f7eb11906152d8592eb1c0b3813c0b21e1c30
5hyun <qhslsl@gmail.com>
2022-09-10 02:06:08 +0900

    Chore: url 아직 설정중

--- components/MypageSidebar/index.tsx
+++ components/MypageSidebar/index.tsx
@@ -1,22 +1,45 @@
 import React from "react";
 import { Info, Title, Wrapper } from "@components/MypageSidebar/styles";
+import { Link, NavLink } from "react-router-dom";
 
 const MypageSidebar = () => {
   return (
     <Wrapper>
-      <Title>마이 페이지</Title>
+      <Title>
+        <Link to={"/mypage"}>마이 페이지</Link>
+      </Title>
 
       <Info>
         <header>쇼핑 정보</header>
-        <span>주문 내역</span>
-        <span>장바구니</span>
+        <span>
+          <NavLink activeClassName="selected" to={"/mypage/buying"}>
+            주문 내역
+          </NavLink>
+        </span>
+        <span>
+          <NavLink activeClassName="selected" to={`/mypage/like`}>
+            장바구니
+          </NavLink>
+        </span>
       </Info>
 
       <Info>
         <header>내 정보</header>
-        <span>프로필 정보</span>
-        <span>주소록</span>
-        <span>결제 정보</span>
+        <span>
+          <NavLink activeClassName="selected" to={`/mypage/profile`}>
+            프로필 정보
+          </NavLink>
+        </span>
+        <span>
+          <NavLink activeClassName="selected" to={`/mypage/address`}>
+            주소록
+          </NavLink>
+        </span>
+        <span>
+          <NavLink activeClassName="selected" to={`/mypage/payment`}>
+            결제 정보
+          </NavLink>
+        </span>
       </Info>
     </Wrapper>
   );
