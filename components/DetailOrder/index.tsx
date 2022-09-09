92f6d4673b38b6c8fd5ad007ff325e3b6d1bf7fe
5hyun <qhslsl@gmail.com>
2022-09-09 16:53:49 +0900

    Feat: 주문 내역 컴포넌트 화

---
+++ components/DetailOrder/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Menu } from "@components/DetailOrder/styles";
+
+const DetailOrder = () => {
+  return (
+    <Menu>
+      <span>주문 내역</span>
+
+      <div>
+        <span>상품정보</span>
+        <span>주문일자</span>
+        <span>주문번호</span>
+        <span>주문금액(수량)</span>
+        <span>주문 상태</span>
+      </div>
+      {/* 컴포넌트로 여기 동적으로 상품 정보 */}
+    </Menu>
+  );
+};
+
+export default DetailOrder;
---
+++ components/DetailOrder/styles.tsx
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const Menu = styled.div`
+  margin-top: 2rem;
+
+  & > span {
+    font-size: 1.3rem;
+  }
+
+  & > div {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+
+    border-top: 2px solid;
+    border-bottom: 1px solid;
+
+    margin-top: 1rem;
+    padding: 0.6rem 0;
+
+    & span {
+      text-align: center;
+    }
+  }
+`;
