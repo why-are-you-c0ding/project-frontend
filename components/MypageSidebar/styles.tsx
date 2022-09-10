d67823b041b29dea3f227858954a7408143b0098
5hyun <qhslsl@gmail.com>
2022-09-11 02:34:33 +0900

    Style: 반응형으로 수정중

--- components/MypageSidebar/styles.tsx
+++ components/MypageSidebar/styles.tsx
@@ -3,24 +3,46 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   margin-left: 2.3rem;
   font-weight: 400;
+
+  @media screen and (max-width: 769px) {
+    margin-bottom: 4rem;
+  }
 `;
 
 export const Title = styled.header`
   font-size: 1.8rem;
   margin-bottom: 1.8rem;
+
+  @media screen and (max-width: 769px) {
+    display: none;
+  }
 `;
 
 export const Info = styled.div`
   & header {
     font-size: 1.5rem;
+
+    @media screen and (max-width: 769px) {
+      display: none;
+    }
   }
 
   display: flex;
   flex-direction: column;
   margin-bottom: 1.5rem;
 
+  @media screen and (max-width: 769px) {
+    margin-bottom: 0;
+    padding-right: 2.3rem;
+  }
+
   & span {
     margin-top: 0.5rem;
     color: rgba(0, 0, 0, 0.4);
+
+    @media screen and (max-width: 769px) {
+      border-bottom: 1px solid rgba(0, 0, 0, 0.4);
+      padding-bottom: 1rem;
+    }
   }
 `;
