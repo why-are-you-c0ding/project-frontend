2cb9fe8401d6c75320f24e652ca7091848da51b7
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-09-14 00:20:26 +0900

    Feat: 장바구니 구현(아직 미완성)

--- components/Like/index.tsx
+++ components/Like/index.tsx
@@ -1,7 +1,70 @@
 import React from "react";
 
+import {
+  BuyBtn,
+  Delete,
+  ItemTitle,
+  ListBox,
+  MiddleSide,
+  RightSide,
+  Wrapper,
+} from "@components/Like/styles";
+import { TopHeader } from "@pages/MyPage/styles";
+
 const Like = () => {
-  return <div>장바구니</div>;
+  return (
+    <div>
+      <Wrapper>
+        <TopHeader>관심 상품</TopHeader>
+        <ListBox>
+          <table>
+            <img
+              src="http://image.dongascience.com/Photo/2022/06/6982fdc1054c503af88bdefeeb7c8fa8.jpg"
+              alt=""
+            />
+            <ItemTitle>
+              <div>강아지</div>
+              <div>레브라도 리트리버</div>
+              <div>짱 귀여워</div>
+            </ItemTitle>
+            <MiddleSide>
+              <div>수량</div>
+            </MiddleSide>
+            <RightSide>
+              <BuyBtn>
+                <div>구매</div>
+                <div>200원</div>
+              </BuyBtn>
+              <Delete>삭제</Delete>
+            </RightSide>
+          </table>
+        </ListBox>
+        <ListBox>
+          <table>
+            <img
+              src="http://image.dongascience.com/Photo/2022/06/6982fdc1054c503af88bdefeeb7c8fa8.jpg"
+              alt=""
+            />
+            <ItemTitle>
+              <div>강아지</div>
+              <div>레브라도 리트리버</div>
+              <div>짱 귀여워</div>
+            </ItemTitle>
+            <MiddleSide>
+              <div>수량</div>
+            </MiddleSide>
+            <RightSide>
+              <BuyBtn>
+                <div>구매</div>
+                <div>200원</div>
+              </BuyBtn>
+              <Delete /**onClick={}**/>삭제</Delete>
+            </RightSide>
+          </table>
+        </ListBox>
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Like;
---
+++ components/Like/styles.tsx
@@ -0,0 +1,94 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.section`
+  max-width: 100rem;
+`;
+
+export const ListBox = styled.div`
+  margin-top: 1rem;
+  border-bottom: 1px solid;
+
+  & table {
+    display: grid;
+    grid-template-columns: 0.5fr 3fr 0.5fr 1.5fr;
+    padding: 0.5rem 0;
+  }
+
+  & img {
+    width: 8rem;
+    height: 8rem;
+  }
+`;
+
+export const ItemTitle = styled.span`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1rem;
+
+  & div:first-child {
+    font-size: 1.3rem;
+    text-decoration: underline;
+    font-weight: bolder;
+  }
+
+  & div:nth-child(2) {
+    font-size: 1rem;
+    margin-top: 0.2rem;
+  }
+
+  & div:last-child {
+    font-size: 1.3rem;
+    font-weight: bolder;
+    position: relative;
+    top: 2.8rem;
+  }
+`;
+
+export const MiddleSide = styled.span`
+  text-align: center;
+`;
+
+export const RightSide = styled.span`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+export const Delete = styled.span`
+  font-size: 1.3rem;
+  text-decoration: underline;
+`;
+
+export const BuyBtn = styled.div`
+  display: flex;
+  width: 11rem;
+  height: 5rem;
+  background-color: red;
+  color: white;
+  border: transparent;
+  border-radius: 20px;
+  text-align: center;
+  outline: none;
+  margin-top: 1rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 2rem;
+  cursor: pointer;
+  &:hover {
+    background-color: #d63535;
+  }
+
+  & div:first-child {
+    display: flex;
+    align-items: center;
+    padding-right: 0.2rem;
+    border-right: 1px solid white;
+    margin-right: 0.5rem;
+  }
+
+  & div:last-child {
+    display: flex;
+    align-items: center;
+    padding: 1rem 1rem;
+  }
+`;
