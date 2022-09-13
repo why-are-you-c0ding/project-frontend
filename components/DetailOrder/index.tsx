fc2d2c11f70cdbc8fd8f0f4886ec75ab155d459d
5hyun <qhslsl@gmail.com>
2022-09-13 19:46:24 +0900

    Chore: 주금 금액 수정

--- components/DetailOrder/index.tsx
+++ components/DetailOrder/index.tsx
@@ -10,7 +10,7 @@ const DetailOrder = () => {
         <span>상품정보</span>
         <span>주문일자</span>
         <span>주문번호</span>
-        <span>주문금액(수량)</span>
+        <span>주문금액</span>
         <span>주문 상태</span>
       </div>
       {/* 컴포넌트로 여기 동적으로 상품 정보 */}
