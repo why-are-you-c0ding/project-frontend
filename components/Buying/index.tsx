4b01abfb0a249ae986a4a7f1d6ebe176b9c22cd1
5hyun <qhslsl@gmail.com>
2022-09-14 16:42:49 +0900

    Chore: 제목 공통 컴포넌트 추가

--- components/Buying/index.tsx
+++ components/Buying/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import DetailOrder from "@components/DetailOrder";
 import ReponsiveBar from "@components/ReponsiveBar";
+import { TopHeader } from "@pages/MyPage/styles";
 
 const Buying = () => {
   return (
     <div>
       <ReponsiveBar title={"주문 내역"} />
+
+      <TopHeader>주문 내역</TopHeader>
 
       <DetailOrder />
     </div>
