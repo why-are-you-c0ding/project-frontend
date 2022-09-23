70cd910b3a855e3aa428e28cef3ded4d3e617d5e
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-09-23 12:18:39 +0900

    Feat: 메인 상품 페이지 링크연결

---
+++ components/Product/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import StatusBar from "@components/StatusBar";
+import { Wrapper } from "@components/Product/styles";
+
+const Product = () => {
+  return (
+    <div>
+      <StatusBar />
+      <Wrapper>
+        <div>하이</div>
+      </Wrapper>
+    </div>
+  );
+};
+
+export default Product;
---
+++ components/Product/styles.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.section`
+  padding-left: 3rem;
+  padding-right: 3rem;
+  padding-top: 2rem;
+`;
