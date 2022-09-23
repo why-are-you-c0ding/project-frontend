37f2300eec15deb95ebdfa627f6160ad90854c64
5hyun <qhslsl@gmail.com>
2022-09-23 15:28:46 +0900

    Chore: 옵션값을 컴포넌트로 뺌

--- components/Option/index.tsx
+++ components/Option/index.tsx
@@ -1,5 +1,6 @@
 import React, {
   ChangeEvent,
+  ChangeEventHandler,
   useCallback,
   useEffect,
   useRef,
@@ -7,41 +8,22 @@ import React, {
 } from "react";
 import { ItemTitle } from "@components/Sell/styles";
 import { Input } from "@pages/SignUp/styles";
-import useInput from "@hooks/useInput";
 import { OptCount, OptInput, OptList } from "@components/Option/styles";
+import OptionInput from "@components/OptionInput";
 
 const Option = () => {
   const optionCount = [1, 2, 3, 4, 5];
   const [count, setCount] = useState(1);
-
-  const [optName0, onChangeOptName0, setOpt0] = useInput("");
-  const [optName1, onChangeOptName1, setOpt1] = useInput("");
-  const [optName2, onChangeOptName2, setOpt2] = useInput("");
-  const [optName3, onChangeOptName3, setOpt3] = useInput("");
-  const [optName4, onChangeOptName4, setOpt4] = useInput("");
+  const [optInput, setOptInput] = useState<string[][]>([]);
 
   const handleCountSelect = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
     const cnt = parseInt(e.target.value);
     setCount(cnt);
   }, []);
 
-  let opt: string[][] = [];
-
   useEffect(() => {
-    for (let i = 0; i < 5; i++) {
-      const t = eval(`optName${i}`);
-
-      if (t) {
-        console.log(t);
-
-        let temp = t;
-        let ary: Array<string> = temp.split(",");
-        console.log(ary);
-        opt.push(ary);
-        console.log(opt);
-      } else break;
-    }
-  });
+    console.log(optInput);
+  }, [optInput]);
 
   return (
     <div>
@@ -78,12 +60,9 @@ const Option = () => {
               {[...Array(count)].map((n, index) => {
                 return (
                   <div key={index}>
-                    <Input
-                      type="text"
-                      placeholder="예시) s, m, l(,로 구분)"
-                      style={{ width: "30vw" }}
-                      value={eval(`optName${index}`)}
-                      onChange={eval(`onChangeOptName${index}`)}
+                    <OptionInput
+                      OptInput={OptInput}
+                      setOptInput={setOptInput}
                     />
                   </div>
                 );
