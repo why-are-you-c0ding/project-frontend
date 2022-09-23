277c9260d3a1044838be6a79c01643e62acfcb45
5hyun <qhslsl@gmail.com>
2022-09-23 15:28:29 +0900

    Feat: 옵션값을 동적으로 추가하기 위한 컴포넌트

---
+++ components/OptionInput/index.tsx
@@ -0,0 +1,62 @@
+import React, {
+  ChangeEvent,
+  DetailedHTMLProps,
+  Dispatch,
+  ElementType,
+  FC,
+  HTMLAttributes,
+  SetStateAction,
+  useCallback,
+  useEffect,
+} from "react";
+import { Input } from "@pages/SignUp/styles";
+import useInput from "@hooks/useInput";
+import { StyledComponent } from "@emotion/styled";
+import { Theme } from "@emotion/react";
+
+interface Props {
+  setOptInput: Dispatch<SetStateAction<string[][]>>;
+  OptInput: StyledComponent<
+    { theme?: Theme | undefined; as?: ElementType<any> | undefined },
+    DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
+    {}
+  >;
+}
+
+const OptionInput: FC<Props> = ({ OptInput, setOptInput }) => {
+  const [optName, , setOpt] = useInput("");
+
+  const onChangeOptName = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setOpt(e.target.value);
+    },
+    [optName]
+  );
+
+  let opt: string[][] = [];
+
+  useEffect(() => {
+    const t = optName.trim();
+
+    console.log(t);
+
+    let temp = t;
+    let ary: Array<string> = temp.split(",");
+    console.log(ary);
+    opt.push(ary);
+  });
+
+  return (
+    <div>
+      <Input
+        type="text"
+        placeholder="예시) s, m, l(,로 구분)"
+        style={{ width: "30vw" }}
+        value={optName}
+        onChange={onChangeOptName}
+      />
+    </div>
+  );
+};
+
+export default OptionInput;
