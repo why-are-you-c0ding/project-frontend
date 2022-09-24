479f858ff580453bfce44ff145b2ab98338e79f2
5hyun <qhslsl@gmail.com>
2022-09-24 21:52:48 +0900

    Style: 위치 변경

--- components/Sell/styles.tsx
+++ components/Sell/styles.tsx
@@ -1,9 +1,7 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.section`
-  padding-left: 3rem;
-  padding-right: 3rem;
-  padding-top: 2rem;
+  margin-top: -8rem;
 `;
 
 export const Image = styled.section`
