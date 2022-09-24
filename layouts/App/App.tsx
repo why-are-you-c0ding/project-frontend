295ac7f9bd19e93936e55cfa86accda3852a3d46
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-09-24 18:23:58 +0900

    Feat: 판매 페이지 경로 설정

--- layouts/App/App.tsx
+++ layouts/App/App.tsx
@@ -6,6 +6,7 @@ const SignUp = loadable(() => import("@pages/SignUp"));
 const LogIn = loadable(() => import("@pages/LogIn"));
 const Main = loadable(() => import(`@layouts/Main`));
 const MyPage = loadable(() => import(`@pages/MyPage`));
+const SellPage = loadable(() => import(`@pages/SellPage`));
 const Shop = loadable(() => import(`@pages/Shop`));
 const SellShop = loadable(() => import(`@pages/SellShop`));
 const ProductPage = loadable(() => import(`@pages/ProductPage`));
@@ -22,6 +23,8 @@ const App = () => {
       <Route path="/login" component={LogIn} />
       <Route path="/mypage" component={MyPage} />
       <Route path="/mypage:id" component={MyPage} />
+      <Route path="/sellpage" component={SellPage} />
+      <Route path="/sellpage:id" component={SellPage} />
       <Route path="/shop" component={Shop} />
       <Route path="/sellshop" component={SellShop} />
       <Route path="/productpage" component={ProductPage} />
