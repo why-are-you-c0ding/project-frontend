aa711372f475cceaee738621309b94be302561f6
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-09-28 15:42:33 +0900

    Feat: 배열안에 맞는 타입 넣기 완료

--- typings/db.ts
+++ typings/db.ts
@@ -3,8 +3,20 @@ export interface EachProduct {
   shopName: string;
   itemId: number;
   itemName: string;
+  basic: boolean;
+  [key: number]: string | number | boolean | any;
+
+  optionGroups: optionGroups[];
+}
+
+export interface optionGroups {
   optionGroupName: string;
-  optionGroupId: Number;
+  optionGroupId: number;
+  [key: string]: string | number | boolean | any;
+  options: { [key: string]: string | number | boolean | any }[];
+}
+
+export interface options {
   optionId: number;
   optionName: string;
   price: number;
