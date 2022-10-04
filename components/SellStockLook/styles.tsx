252f7b14811ae182861e2c4b56d42a0f8fedb562
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-04 21:49:07 +0900

    Feat: 재고 조회 구현

---
+++ components/SellStockLook/styles.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div``;
+export const StockOption = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const StockID = styled.div`
+  display: flex;
+  flex-direction: column;
+  &h1 {
+  }
+`;
+export const StockNumber = styled.div`
+  display: flex;
+  flex-direction: column;
+  &h1 {
+  }
+`;
