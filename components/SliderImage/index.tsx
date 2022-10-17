1fb2e78cc041d5ea81172068076f8d061adde41b
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-17 23:19:48 +0900

    Feat: 슬라이드 컴포넌트 구현 완료

---
+++ components/SliderImage/index.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { SliderItem } from "@components/SliderImage/styles";
+import { slideList } from "@utils/slideList";
+
+const SliderImage = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div>
+      <h2> Single Item</h2>
+      <Slider {...settings}>
+        {slideList.map((v, index) => (
+          <SliderItem key={index}>
+            <img src={v.src} alt={v.alt} />
+          </SliderItem>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default SliderImage;
---
+++ components/SliderImage/styles.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const SliderItem = styled.div`
+  width: 100%;
+  img {
+    width: 50rem;
+    max-width: 100%;
+    height: auto;
+  }
+`;
