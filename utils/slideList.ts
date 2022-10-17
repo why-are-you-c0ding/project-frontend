8ade8ffdd5a31fd8b3ef035b4fb3082a73e54335
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-17 23:19:37 +0900

    Feat: 파일명 수정

--- utils/slideList.ts
+++ utils/slideList.ts
@@ -1,22 +1,17 @@
 export const slideList = [
   {
     id: 1,
-    src: "slideImage/img1.jpeg",
+    src: "slideImage/img1.jpg",
     alt: "첫번째 슬라이드",
   },
   {
     id: 2,
-    src: "slideImage/img2.jpeg",
+    src: "slideImage/img2.jpg",
     alt: "두번째 슬라이드",
   },
   {
     id: 3,
-    src: "slideImage/img3.jpeg",
-    alt: "세번째 슬라이드",
-  },
-  {
-    id: 4,
-    src: "slideImage/img2.jpeg",
+    src: "slideImage/img3.jpg",
     alt: "세번째 슬라이드",
   },
 ];
