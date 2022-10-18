b8c098706447b92d8acbe026feb6055ff3ff2342
5hyun <qhslsl@gmail.com>
2022-10-18 22:59:34 +0900

    Chore: 슬라이드 크기 수정 및 잠시 느리게 흘러가도록 함

--- components/SliderImage/index.tsx
+++ components/SliderImage/index.tsx
@@ -1,32 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, useCallback, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { SliderItem } from "@components/SliderImage/styles";
+import { SliderItem, Wrapper } from "@components/SliderImage/styles";
 import { slideList } from "@utils/slideList";
 
 const SliderImage = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 2000000,
     autoplay: true,
     autoplaySpeed: 4000,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
 
+  const a = useCallback(() => {}, []);
+
   return (
-    <div>
-      <h2> Single Item</h2>
-      <Slider {...settings}>
+    <Wrapper>
+      <Slider {...settings} className={"slide"}>
         {slideList.map((v, index) => (
           <SliderItem key={index}>
             <img src={v.src} alt={v.alt} />
           </SliderItem>
         ))}
       </Slider>
-    </div>
+    </Wrapper>
   );
 };
 
--- components/SliderImage/styles.tsx
+++ components/SliderImage/styles.tsx
@@ -1,10 +1,19 @@
 import styled from "@emotion/styled";
 
+export const Wrapper = styled.section`
+  margin-top: -2rem;
+
+  .slide {
+    margin: 0 auto;
+
+    height: 24rem;
+    width: 85vw;
+  }
+`;
+
 export const SliderItem = styled.div`
-  width: 100%;
   img {
-    width: 50rem;
-    max-width: 100%;
-    height: auto;
+    width: 100%;
+    height: 100%;
   }
 `;
