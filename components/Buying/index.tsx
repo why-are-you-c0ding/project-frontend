695b5bbcf3121ba5b75dc6458399712feaa7d9ff
5hyun <qhslsl@gmail.com>
2022-10-20 20:14:11 +0900

    Feat: 무한 스크롤 기본 틀 완성

--- components/Buying/index.tsx
+++ components/Buying/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import DetailOrder from "@components/DetailOrder";
 import ReponsiveBar from "@components/ReponsiveBar";
 import { TopHeader } from "@pages/MyPage/styles";
@@ -8,17 +8,45 @@ import {
   InfoTop,
   ItemInfo,
 } from "@components/Like/styles";
+import useSWRInfinite from "swr/infinite";
+import fetcher from "@utils/fetcher";
+
+const PAGE_SIZE = 10;
 
 const Buying = () => {
+  const { data: orderData, setSize } = useSWRInfinite<any>(
+    (index) => `https://waycabvav.shop/orders/customers?page=${index}`,
+    fetcher
+  );
+
+  const orderList = orderData ? [].concat(...orderData) : [];
+  // const isLoadingInitialData = !orderData && !error;
+  // const isLoadingMore =
+  //   isLoadingInitialData ||
+  //   (size > 0 && orderData && typeof orderData[size - 1] === "undefined");
+  // const isEmpty = orderData?.[0]?.length === 0;
+  // const isReachingEnd =
+  //   isEmpty ||
+  //   (orderData && orderData[orderData.length - 1]?.length < PAGE_SIZE);
+  // const isRefreshing = isValidating && orderData && orderData.length === size;
+
+  console.log("이게 리스트1: ", orderList);
+  if (orderData) console.log("Data: ", orderData[0].orders.length);
+  if (orderData) console.log("List: ", orderList);
+
+  const onClick2 = useCallback(() => {
+    setSize((prev) => prev + 1);
+  }, []);
+
   return (
     <div>
       <ReponsiveBar title={"주문 내역"} />
 
       <TopHeader>주문 내역</TopHeader>
 
-      {/*<DetailOrder />*/}
-
       <div style={{ marginTop: "6rem", padding: "0 2rem" }}>
+        <button onClick={onClick2}>버튼2</button>
+
         <CartItem>
           <img
             src="http://image.dongascience.com/Photo/2022/06/6982fdc1054c503af88bdefeeb7c8fa8.jpg"
