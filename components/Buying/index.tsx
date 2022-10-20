a15bf3df1cf91e4bd778d798dddfa70cdfee5009
5hyun <qhslsl@gmail.com>
2022-10-21 00:31:33 +0900

    Feat: 무한 스크롤 구현은 성공

--- components/Buying/index.tsx
+++ components/Buying/index.tsx
@@ -1,5 +1,10 @@
-import React, { useCallback } from "react";
-import DetailOrder from "@components/DetailOrder";
+import React, {
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import ReponsiveBar from "@components/ReponsiveBar";
 import { TopHeader } from "@pages/MyPage/styles";
 import {
@@ -10,80 +15,99 @@ import {
 } from "@components/Like/styles";
 import useSWRInfinite from "swr/infinite";
 import fetcher from "@utils/fetcher";
-
-const PAGE_SIZE = 10;
+import { Scrollbars } from "react-custom-scrollbars";
 
 const Buying = () => {
-  const { data: orderData, setSize } = useSWRInfinite<any>(
+  const {
+    data: orderData,
+    size,
+    setSize,
+  } = useSWRInfinite<any>(
     (index) => `https://waycabvav.shop/orders/customers?page=${index}`,
     fetcher
   );
 
-  const orderList = orderData ? [].concat(...orderData) : [];
-  // const isLoadingInitialData = !orderData && !error;
-  // const isLoadingMore =
-  //   isLoadingInitialData ||
-  //   (size > 0 && orderData && typeof orderData[size - 1] === "undefined");
-  // const isEmpty = orderData?.[0]?.length === 0;
-  // const isReachingEnd =
-  //   isEmpty ||
-  //   (orderData && orderData[orderData.length - 1]?.length < PAGE_SIZE);
-  // const isRefreshing = isValidating && orderData && orderData.length === size;
+  let orderList: any = [];
 
-  console.log("이게 리스트1: ", orderList);
-  if (orderData) console.log("Data: ", orderData[0].orders.length);
-  if (orderData) console.log("List: ", orderList);
+  if (orderData) {
+    for (let i = 0; i < orderData?.length; i++) {
+      orderList.push(orderData[i].orders);
+    }
+  }
 
-  const onClick2 = useCallback(() => {
-    setSize((prev) => prev + 1);
+  const onClick = useCallback(() => {
+    if (!orderData?.[orderList.length - 1]?.finalPage)
+      setSize((prev) => prev + 1);
+  }, [orderData]);
+
+  orderList = orderList.flat();
+
+  const scrollbarRef = useRef<Scrollbars>(null);
+
+  const onScroll = useCallback((values: any) => {
+    const current = (scrollbarRef as MutableRefObject<Scrollbars>)?.current;
+
+    // if (current) console.log(current.getValues().top);
+    if (current) {
+      if (current.getValues().top === 1) {
+        setSize((prev) => prev + 1);
+      }
+    }
   }, []);
 
   return (
     <div>
-      <ReponsiveBar title={"주문 내역"} />
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        <ReponsiveBar title={"주문 내역"} />
 
-      <TopHeader>주문 내역</TopHeader>
+        <TopHeader>주문 내역</TopHeader>
 
-      <div style={{ marginTop: "6rem", padding: "0 2rem" }}>
-        <button onClick={onClick2}>버튼2</button>
+        <div style={{ marginTop: "6rem", padding: "0 2rem" }}>
+          {orderData &&
+            orderList &&
+            orderList?.map((v: any, index: number) => {
+              return (
+                <div key={index}>
+                  <CartItem>
+                    <img src={v.itemImageUrl} alt="" />
+                    <ItemInfo>
+                      <InfoTop>
+                        <div>
+                          <span>{v.itemName}</span>
+                          {/*<span>{v.}</span>*/}
+                        </div>
+                        <div></div>
+                      </InfoTop>
+                      <InfoBottom>
+                        <div>
+                          <span>2~3일 내 도착</span>
+                        </div>
+                        <div>
+                          <div>
+                            <span>2개</span>
+                          </div>
 
-        <CartItem>
-          <img
-            src="http://image.dongascience.com/Photo/2022/06/6982fdc1054c503af88bdefeeb7c8fa8.jpg"
-            alt=""
-          />
-          <ItemInfo>
-            <InfoTop>
-              <div>
-                <span>아이템 이름</span>
-                <span>10000원</span>
-              </div>
-              <div></div>
-            </InfoTop>
-            <InfoBottom>
-              <div>
-                <span>2~3일 내 도착</span>
-              </div>
-              <div>
-                <div>
-                  <span>2개</span>
+                          <div>
+                            <span>총 금액: 20000원</span>
+                            <span>
+                              <button onClick={(event) => {}}>
+                                <span
+                                  style={{ fontSize: "1rem", color: "red" }}
+                                >
+                                  주문 취소
+                                </span>
+                              </button>
+                            </span>
+                          </div>
+                        </div>
+                      </InfoBottom>
+                    </ItemInfo>
+                  </CartItem>
                 </div>
-
-                <div>
-                  <span>총 금액: 20000원</span>
-                  <span>
-                    <button onClick={(event) => {}}>
-                      <span style={{ fontSize: "1rem", color: "red" }}>
-                        주문 취소
-                      </span>
-                    </button>
-                  </span>
-                </div>
-              </div>
-            </InfoBottom>
-          </ItemInfo>
-        </CartItem>
-      </div>
+              );
+            })}
+        </div>
+      </Scrollbars>
     </div>
   );
 };
