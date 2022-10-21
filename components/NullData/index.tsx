c2e8f84031df26b18a81fec5a95a0897d7805c49
5hyun <qhslsl@gmail.com>
2022-10-21 17:13:15 +0900

    Feat: 데이터가 없을 때 사용 할 컴포넌트

---
+++ components/NullData/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Null } from "@components/NullData/styles";
+
+const NullData = () => {
+  return <Null>텅~</Null>;
+};
+
+export default NullData;
---
+++ components/NullData/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Null = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3rem;
+  color: rgba(0, 0, 0, 0.4);
+
+  height: 50vh;
+`;
