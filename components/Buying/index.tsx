0f0430c93ee88c95f8687600c61f2a80ece57b90
5hyun <qhslsl@gmail.com>
2022-10-21 21:21:14 +0900

    Style: 주문 내역 스타일 수정

--- components/Buying/index.tsx
+++ components/Buying/index.tsx
@@ -1,10 +1,4 @@
-import React, {
-  MutableRefObject,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import React, { useEffect } from "react";
 import ReponsiveBar from "@components/ReponsiveBar";
 import { TopHeader } from "@pages/MyPage/styles";
 import {
@@ -36,11 +30,6 @@ const Buying = () => {
     }
   }
 
-  const onClick = useCallback(() => {
-    if (!orderData?.[orderList.length - 1]?.finalPage)
-      setSize((prev) => prev + 1);
-  }, [orderData]);
-
   orderList = orderList.flat();
 
   const [ref, inView] = useInView();
@@ -53,44 +42,36 @@ const Buying = () => {
   }, [inView]);
 
   return (
-    <Wrapper id="sc">
-      <ReponsiveBar title={"주문 내역"} />
+    <div>
+      <ReponsiveBar title={"장바구니"} />
+      <Wrapper>
+        <TopHeader>주문 내역</TopHeader>
 
-      <TopHeader>주문 내역</TopHeader>
-
-      <div style={{ marginTop: "6rem", padding: "0 2rem" }}>
         {orderData &&
           orderList &&
           orderList?.map((v: any, index: number) => {
             return index === orderList.length - 3 ? (
               <CartItem key={index} ref={ref}>
-                <img src={v.itemImageUrl} alt="" />
+                <img src={v.itemImageUrl} alt={v.itemName} />
                 <ItemInfo>
                   <InfoTop>
-                    <div>
+                    <div style={{ paddingBottom: "2.5rem" }}>
                       <span>{v.itemName}</span>
                       <span>{v.shopName}</span>
                     </div>
-                    <div></div>
                   </InfoTop>
-                  <InfoBottom>
+                  <InfoBottom style={{ marginTop: "0" }}>
                     <div>
                       <span>2~3일 내 도착</span>
                     </div>
                     <div>
                       <div>
-                        <span>2개</span>
+                        <span>수량: {v.count}개</span>
                       </div>
 
                       <div>
-                        <span style={{}}>총 금액: 20000원</span>
-                        {/*<span>*/}
-                        {/*  <button onClick={(event) => {}}>*/}
-                        {/*    <span style={{ fontSize: "1rem", color: "red" }}>*/}
-                        {/*      주문 취소*/}
-                        {/*    </span>*/}
-                        {/*  </button>*/}
-                        {/*</span>*/}
+                        <span>{v.price}원</span>
+                        <span></span>
                       </div>
                     </div>
                   </InfoBottom>
@@ -103,19 +84,8 @@ const Buying = () => {
                   <InfoTop>
                     <div style={{ paddingBottom: "2.5rem" }}>
                       <span>{v.itemName}</span>
-                      <span>{v.itemName}원</span>
+                      <span>{v.shopName}</span>
                     </div>
-                    {/*<div style={{ color: "rgba(0,0,0,0.4)" }}>*/}
-                    {/*  /!*{item[index]?.cartOptionGroups[0]?.cartOptions[0]?.name}*!/*/}
-                    {/*  /!*&nbsp;&nbsp;*!/*/}
-                    {/*  /!*{item[index]?.cartOptionGroups[1]?.cartOptions[0]?.name}*!/*/}
-                    {/*  /!*&nbsp;&nbsp;*!/*/}
-                    {/*  /!*{item[index]?.cartOptionGroups[2]?.cartOptions[0]?.name}*!/*/}
-                    {/*  /!*&nbsp;&nbsp;*!/*/}
-                    {/*  /!*{item[index]?.cartOptionGroups[3]?.cartOptions[0]?.name}*!/*/}
-                    {/*  /!*&nbsp;&nbsp;*!/*/}
-                    {/*  /!*{item[index]?.cartOptionGroups[4]?.cartOptions[0]?.name}*!/*/}
-                    {/*</div>*/}
                   </InfoTop>
                   <InfoBottom style={{ marginTop: "0" }}>
                     <div>
@@ -123,11 +93,11 @@ const Buying = () => {
                     </div>
                     <div>
                       <div>
-                        <span>{v.itemName}개</span>
+                        <span>수량: {v.count}개</span>
                       </div>
 
                       <div>
-                        <span>{v.itemName}원</span>
+                        <span>{v.price * v.count}원</span>
                         <span></span>
                       </div>
                     </div>
@@ -136,8 +106,8 @@ const Buying = () => {
               </CartItem>
             );
           })}
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </div>
   );
 };
 
