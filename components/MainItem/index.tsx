33e5a6204274d694f5dcfe538c701c0aca17d705
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-25 18:31:03 +0900

    Feat: 전체 상품 조회 구현

--- components/MainItem/index.tsx
+++ components/MainItem/index.tsx
@@ -13,27 +13,27 @@ import {
 import useSWR from "swr";
 import { Link } from "react-router-dom";
 import fetcher_noneHeaders from "@utils/fetcher_noneHeaders";
-import { AllData } from "@typings/db";
+// import { AllData } from "@typings/db";
 
 const MainItem = () => {
-  const { data: allData, error } = useSWR<AllData[]>(
-    "https://waycabvav.shop/items",
+  const { data: allData, error } = useSWR(
+    "https://waycabvav.shop/items?page=0",
     fetcher_noneHeaders
   );
 
-  let itemList: AllData[] = [];
-
-  if (allData) itemList = Object.values(allData);
-
-  let item: Array<Array<string | number>> = [];
-
-  for (let i = 0; i < itemList.length; i++) {
-    item[i] = Object.values(itemList[i]);
-  }
-
-  if (!allData) {
-    return <div>로딩중입니다...</div>;
-  }
+  // let itemList: AllData[] = [];
+  //
+  // if (allData) itemList = Object.values(allData);
+  //
+  // let item: Array<Array<string | number>> = [];
+  //
+  // for (let i = 0; i < itemList.length; i++) {
+  //   item[i] = Object.values(itemList[i]);
+  // }
+  //
+  // if (!allData) {
+  //   return <div>로딩중입니다...</div>;
+  // }
 
   return (
     <Wrapper>
@@ -41,27 +41,24 @@ const MainItem = () => {
         <h2>전체 상품</h2>
       </TitleContainer>
       <ItemContainer>
-        {
-          // allData
-          // &&
-          allData?.map((v, index) => {
-            const itemId = item[index][0];
+        {allData?.items &&
+          allData?.items.map((v: any, index: any) => {
+            const itemId = allData?.items[index].itemId;
 
             return (
               <Link to={`/shop/${itemId}`} key={index}>
                 <ItemBox>
                   <ItemImg>
-                    <img src={v.imageUrl} alt="으악" />
+                    <img src={allData?.items[index].imageUrl} alt="으악" />
                   </ItemImg>
                   <ItemInfo>
-                    <ItemName>{item[index][1]}</ItemName>
-                    <ItemPrice>{item[index][3]}원</ItemPrice>
+                    <ItemName>{allData?.items[index].itemName}</ItemName>
+                    <ItemPrice>{allData?.items[index].basicPrice}원</ItemPrice>
                   </ItemInfo>
                 </ItemBox>
               </Link>
             );
-          })
-        }
+          })}
       </ItemContainer>
     </Wrapper>
   );
