6035ddc2b408f1ef30e7624ac05ade9cc3fa7d64
5hyun <qhslsl@gmail.com>
2022-11-26 23:07:36 +0900

    Chore: 안쓰는 것들 제거

--- layouts/Main/index.tsx
+++ layouts/Main/index.tsx
@@ -1,13 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import StatusBar from "@components/StatusBar";
 import SliderImage from "@components/SliderImage";
 
-import Recommend from "@components/Recommend";
 import MainItem from "@components/MainItem";
 
 const Main = () => {
-  const [isLogin, setIsLogin] = useState(localStorage.getItem("jwt") !== null);
-
   return (
     <div>
       <StatusBar />
