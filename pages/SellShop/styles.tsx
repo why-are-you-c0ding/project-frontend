5981ce9fb13857982698f48f01055ab112ebc194
5hyun <qhslsl@gmail.com>
2022-12-20 01:36:23 +0900

    Feat: 파일 삭제

--- pages/SellShop/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Wrapper = styled.div`
-  display: block;
-  max-width: 1280px;
-  margin: 0 auto;
-  margin-top: 5rem;
-  margin-bottom: 5rem;
-`;
