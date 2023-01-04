497c8e4393e5af711be5fe549909001fce0d6b3e
5hyun <qhslsl@gmail.com>
2023-01-05 03:57:12 +0900

    Feat: 상품 등록 페이지에서 옵션을 가공하는 함수가 들은 파일

---
+++ utils/makeOptionTableList.ts
@@ -0,0 +1,36 @@
+import { ItemInfo } from "@components/SignUpItemBodys/SellOption";
+
+export interface Options {
+  optionName: string;
+  price: string;
+}
+
+export interface Option {
+  options: Options[];
+  optionGroupName: string;
+  basic: boolean;
+}
+
+export const makeOptionTableList = (itemInfos: ItemInfo[]) => {
+  let list: Option[] = [];
+
+  for (let i = 0; i < itemInfos.length; i++) {
+    let item: Option = {
+      optionGroupName: itemInfos[i].name,
+      basic: i === 0,
+      options: [],
+    };
+
+    let temp = itemInfos[i].values.split(",");
+    temp = temp.filter((v) => v !== "");
+    for (let i = 0; i < temp.length; i++) {
+      item.options.push({
+        optionName: temp[i].trim(),
+        price: "",
+      });
+    }
+    list.push(item);
+  }
+
+  return list;
+};
