b31bfded3da8bb7d58c707bebb459696a9e70044
5hyun <qhslsl@gmail.com>
2023-01-05 03:55:09 +0900

    Feat: 빼먹은 리덕스 설정 추가

--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import { sellOptionSlice } from "./sellOptionSlice";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  sellOption: sellOptionSlice.reducer,
+});
 
 export default reducer;
