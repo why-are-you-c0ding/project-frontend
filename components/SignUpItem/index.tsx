66ea97b69c1b0213d221c59255d41f98d2d76769
5hyun <qhslsl@gmail.com>
2023-02-11 03:43:14 +0900

    Feat: 기존 이미지 입력 삭제

--- components/SignUpItem/index.tsx
+++ components/SignUpItem/index.tsx
@@ -8,21 +8,19 @@ import React, {
 import { TopHeader } from "@pages/MyPage/styles";
 import ReponsiveBar from "@components/ReponsiveBar";
 import {
-  DragOver,
   EachWrapper,
   Image,
   ItemInfo,
   ItemTitle,
   OptionInfo,
-  Preview,
   Textarea,
   Wrapper,
 } from "@components/SignUpItem/styles";
 import { Input } from "@pages/SignUp/styles";
 import SellOption from "@components/SignUpItemBodys/SellOption";
 import useInput from "@hooks/useInput";
-import axios from "axios";
 import autosize from "autosize";
+import SellOptionImg from "@components/SignUpItemBodys/SellOptionImg";
 
 const SignUpItem = () => {
   const categoryList = [
@@ -46,7 +44,7 @@ const SignUpItem = () => {
     "Premium Beauty",
   ];
 
-  const [itemName, onChangeItemname, setItemName] = useInput("");
+  const [itemName, onChangeItemName, setItemName] = useInput("");
   const [explain, , setItemExplain] = useInput("");
   const [category, setCategory] = useState(categoryList[0]);
 
@@ -60,82 +58,6 @@ const SignUpItem = () => {
     },
     []
   );
-
-  const [files, setFiles] = useState("");
-  const [imageSrc, setImageSrc] = useState("");
-  const [dragOver, setDragOver] = useState(true);
-
-  const encodeFileToBase64 = (fileBlob: Blob) => {
-    const reader: any = new FileReader();
-
-    reader.readAsDataURL(fileBlob);
-
-    return new Promise<void>((resolve) => {
-      reader.onload = () => {
-        if (reader.result) setImageSrc(reader.result);
-        setDragOver(false);
-
-        resolve();
-      };
-    });
-  };
-
-  const onLoadFile = (e: any) => {
-    const file = e.target.files;
-    setFiles(file);
-
-    encodeFileToBase64(e.target.files[0]);
-  };
-
-  const [imageUrl, setImageUrl] = useState("");
-
-  useEffect(() => {
-    if (files && imageSrc) {
-      const formdata = new FormData();
-      formdata.append("images", files[0]);
-
-      axios
-        .post("https://waycabvav.shop/images", formdata, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .then((response) => {
-          setImageUrl(response.data.imageUrl);
-          console.log(response);
-        })
-        .catch((error) => {});
-    }
-  }, [files, imageSrc]);
-
-  const formData = new FormData();
-
-  const onDrop = useCallback((e: any) => {
-    e.preventDefault();
-    if (e.dataTransfer.item) {
-      for (let i = 0; i < e.dataTransfer.items.length; i++) {
-        // If dropped items aren't files, reject them
-        if (e.dataTransfer.items[i].kind === "file") {
-          const file = e.dataTransfer.items[i].getAsFile();
-          formData.append("images", file);
-        }
-      }
-    } else {
-      for (let i = 0; i < e.dataTransfer.files.length; i++) {
-        formData.append("images", e.dataTransfer.files[i]);
-      }
-    }
-
-    setFiles(e.dataTransfer.files);
-
-    encodeFileToBase64(e.dataTransfer.files[0]);
-  }, []);
-
-  const onDragOver = useCallback((e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setDragOver(true);
-  }, []);
 
   const ref = useRef<HTMLTextAreaElement>(null);
 
@@ -155,33 +77,7 @@ const SignUpItem = () => {
         <EachWrapper>
           <Image>
             <h3>등록 상품 이미지</h3>
-
-            <div>
-              <Preview onDrop={onDrop} onDragOver={onDragOver}>
-                {dragOver && (
-                  <DragOver>
-                    <div>상품 사진</div>
-                  </DragOver>
-                )}
-                {imageSrc && <img src={imageSrc} alt="preview-img" />}
-              </Preview>
-
-              {dragOver && (
-                <span>사진(jpeg만 가능)은 1:1 비율을 권장합니다.</span>
-              )}
-
-              <div>
-                <label htmlFor="file">
-                  <div>파일 업로드</div>
-                </label>
-                <input
-                  type="file"
-                  id="file"
-                  accept="image/jpeg"
-                  onChange={onLoadFile}
-                />
-              </div>
-            </div>
+            <SellOptionImg />
           </Image>
         </EachWrapper>
 
@@ -196,7 +92,7 @@ const SignUpItem = () => {
                     type="text"
                     name="item-name"
                     value={itemName}
-                    onChange={onChangeItemname}
+                    onChange={onChangeItemName}
                     placeholder="상품 이름을 입력해주세요."
                   />
                 </label>
