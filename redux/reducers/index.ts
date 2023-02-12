07d84ea2efdfa3a8586ca09d95c17d2a1f73c47a
5hyun <qhslsl@gmail.com>
2023-02-12 09:28:05 +0900

    Feat: userInfoSlice 생성

--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { signUpItemSlice } from "./signUpItemSlice";
+import { userInfoSlice } from "./userInfoSlice";
 
 const reducer = combineReducers({
   sellOption: signUpItemSlice.reducer,
+  userInfo: userInfoSlice.reducer,
 });
 
 export default reducer;
