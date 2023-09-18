e32aa5284b4c40b33bd9f85726cf40ef006dfba2
5hyun <qhslsl@gmail.com>
2023-09-19 01:56:13 +0900

    Fix: 리액트 17에서 18로 올려서 생기는 렌더링 문제 해결

--- client.tsx
+++ client.tsx
@@ -1,22 +1,19 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import axios from "axios";
 
 import App from "@layouts/App/App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "https://waycabvav.shop";
+const root = ReactDOM.createRoot(document.querySelector("#app") as HTMLElement);
 
-render(
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.querySelector("#app")
 );
