e08246956401f41b51d2eb792d8a53e1ab8664fa
5hyun <qhslsl@gmail.com>
2023-09-19 19:56:10 +0900

    Feat: 회원가입 API 설정

--- api/memberApi.ts
+++ api/memberApi.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { SignUpInfo } from "@typings/member";
 const URL = process.env.REACT_APP_BASE_URL;
 
 export const memberApi = createApi({
@@ -12,6 +13,74 @@ export const memberApi = createApi({
           url: "/verification/login-id",
           method: "POST",
           body: { loginId },
+        };
+      },
+    }),
+    receiveEmail: builder.mutation({
+      query: ({ receiveEmail }: { receiveEmail: string }) => {
+        return {
+          url: "/emails",
+          method: "POST",
+          body: { receiveEmail },
+        };
+      },
+    }),
+    verificationNickName: builder.mutation({
+      query: ({ nickName }: { nickName: string }) => {
+        return {
+          url: "/verification/nick-name",
+          method: "POST",
+          body: { nickName },
+        };
+      },
+    }),
+    signUpConsumers: builder.mutation({
+      query: ({
+        nickName,
+        email,
+        loginId,
+        password,
+        checkPassword,
+        age,
+        authKey,
+      }: SignUpInfo) => {
+        return {
+          url: "/members/consumers",
+          method: "POST",
+          body: {
+            nickName,
+            email,
+            loginId,
+            password,
+            checkPassword,
+            age,
+            authKey,
+          },
+        };
+      },
+    }),
+    signUSellers: builder.mutation({
+      query: ({
+        nickName,
+        email,
+        loginId,
+        password,
+        checkPassword,
+        age,
+        authKey,
+      }: SignUpInfo) => {
+        return {
+          url: "/members/sellers",
+          method: "POST",
+          body: {
+            nickName,
+            email,
+            loginId,
+            password,
+            checkPassword,
+            age,
+            authKey,
+          },
         };
       },
     }),
