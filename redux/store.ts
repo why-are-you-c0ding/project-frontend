49411fdd66ba1ee51ddd9332d56f11780cb95040
5hyun <qhslsl@gmail.com>
2023-09-20 19:23:06 +0900

    Feat: myPageApi 추가

--- redux/store.ts
+++ redux/store.ts
@@ -1,14 +1,16 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import { memberApi } from "../api/memberApi";
+import { myPageApi } from "@api/myPageApi";
+import { memberApi } from "@api/memberApi";
 
 export const store = configureStore({
   reducer: {
     [memberApi.reducerPath]: memberApi.reducer,
+    [myPageApi.reducerPath]: myPageApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat(memberApi.middleware),
+    }).concat(memberApi.middleware, myPageApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
