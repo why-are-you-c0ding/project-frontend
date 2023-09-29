037c89c7d1391ea396349f1ab741716e0dc7cb55
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-09-29 21:46:09 +0900

    feat: 목데이터 삽입, 파일 분리

---
+++ mock/api/data/member/myPage.ts
@@ -0,0 +1,37 @@
+export const cartData = {
+  cartLineItems: [
+    {
+      id: 20,
+      itemId: 29,
+      name: "멋쟁이 옷",
+      count: 2,
+      cartOptionGroups: [
+        {
+          id: 11,
+          cartOption: {
+            name: "Large",
+            price: 2000,
+          },
+          name: "기초 옵션",
+        },
+        {
+          id: 12,
+          cartOption: {
+            name: "Black",
+            price: 1000,
+          },
+          name: "Color",
+        },
+        {
+          id: 13,
+          cartOption: {
+            name: "Large",
+            price: 2000,
+          },
+          name: "Sizee",
+        },
+      ],
+      imageUrl: "www.image.url",
+    },
+  ],
+};
--- mock/api/member/myPage.ts
+++ mock/api/member/myPage.ts
@@ -1,44 +1,7 @@
 import { rest } from "msw";
+import { cartData } from "@mock/api/data/member/myPage";
 
-const cartData = {
-  cartLineItems: [
-    {
-      id: 20,
-      itemId: 29,
-      name: "멋쟁이 옷",
-      count: 2,
-      cartOptionGroups: [
-        {
-          id: 11,
-          cartOption: {
-            name: "Large",
-            price: 2000,
-          },
-          name: "기초 옵션",
-        },
-        {
-          id: 12,
-          cartOption: {
-            name: "Black",
-            price: 1000,
-          },
-          name: "Color",
-        },
-        {
-          id: 13,
-          cartOption: {
-            name: "Large",
-            price: 2000,
-          },
-          name: "Sizee",
-        },
-      ],
-      imageUrl: "www.image.url",
-    },
-  ],
-};
-
-export const myPageHandlers = [
+export const myPage = [
   // 장바구니 조회 요청
   rest.get("/carts", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(cartData));
