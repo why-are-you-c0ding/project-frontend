862d24c4a32e437f88d3f61a70db9c2f8e00bd0a
5hyun <qhslsl@gmail.com>
2023-09-29 16:49:33 +0900

    Chore: 절대 경로로 바꿈

--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import { signUpItemSlice } from "./signUpItemSlice";
-import { userInfoSlice } from "./userInfoSlice";
+import { userInfoSlice } from "@redux/reducers/userInfoSlice";
+import { signUpItemSlice } from "@redux/reducers/signUpItemSlice";
 
 const reducer = combineReducers({
   sellOption: signUpItemSlice.reducer,
