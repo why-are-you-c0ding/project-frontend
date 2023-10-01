d88a57e1554cee8ee5872a97a49c3fe07bf23318
5hyun <qhslsl@gmail.com>
2023-10-01 17:38:08 +0900

    Feat: 회원가입 요청 보냈을 때, 에러 처리 mock 데이터 추가

--- mock/api/data/member/signUp.ts
+++ mock/api/data/member/signUp.ts
@@ -3,3 +3,22 @@ export const validateId = {
   // message: "검증에 실패했습니다.",
 };
 export const verificationNickname = { message: "검증에 성공했습니다." };
+
+export const receiveEmail = { message: "이메일 발송에 성공했습니다." };
+
+export const signupMember = {
+  // 회원가입 성공
+  message: "멤버 생성에 성공했습니다.",
+
+  // 이메일 인증 번호를 안넣었을때
+  // message: "인증을 진행할 수 없는 이메일입니다..",
+
+  // 이메일 인증 번호 전송을 했는데 인증 번호가 틀렸을 때
+  //   message: "이메일 인증번호가 틀립니다."
+
+  //   닉네임 중복
+  // message: "해당 닉네임은 이미 존재하는 닉네임입니다.",
+
+  //   로그인 아이디 중복
+  // message: "해당 로그인 아이디는 이미 존재하는 로그인 아이디입니다.",
+};
