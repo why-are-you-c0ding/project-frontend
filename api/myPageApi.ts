9bf99ade18b11f2f0398e953c8a605655079fe73
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-10-02 21:36:37 +0900

    feat: mock데이터로 통신 구현

--- api/myPageApi.ts
+++ api/myPageApi.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { CartItemInfo } from "@typings/myPage";
 const URL = process.env.REACT_APP_BASE_URL;
 const isDevelopment = process.env.NODE_ENV !== "development";
 
@@ -7,46 +8,37 @@ export const myPageApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: isDevelopment ? URL : "/" }),
   tagTypes: ["cart"],
   endpoints: (builder) => ({
-    getCount: builder.query({
-      query: () => "/carts",
-      providesTags: (result, error, arg) => {
-        return [{ type: "cart", id: arg.name }];
-      },
-    }),
     getCart: builder.query({
+      query: () => "/carts",
+    }),
+    getCount: builder.query({
       query: () => "/carts/current",
       providesTags: (result, error, arg) => {
         return [{ type: "cart", id: "current" }];
       },
     }),
     deleteCartItem: builder.mutation({
-      query: ({ cartLineItemId }) => ({
-        url: `/carts/cart-line-items/${cartLineItemId}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: (result, error, arg) => {
-        return [{ type: "cart", id: "current" }];
+      query: ({ cartLineItemId }: { cartLineItemId: number }) => {
+        return {
+          url: "/carts/cart-line-items",
+          method: "delete",
+          body: { cartLineItemId },
+        };
       },
     }),
     downCartItem: builder.mutation({
-      query: ({ cartLineItemId, count }) => ({
-        url: `/carts/cart-line-items/${cartLineItemId}`,
+      query: ({ cartLineItemId, count }: CartItemInfo) => ({
+        url: `/carts/cart-line-items`,
         method: "PATCH",
-        body: { count },
+        body: { cartLineItemId, count },
       }),
-      invalidatesTags: (result, error, arg) => {
-        return [{ type: "cart", id: "current" }];
-      },
     }),
     upCartItem: builder.mutation({
-      query: ({ cartLineItemId, count }) => ({
-        url: `/carts/cart-line-items/${cartLineItemId}`,
+      query: ({ cartLineItemId, count }: CartItemInfo) => ({
+        url: `/carts/cart-line-items`,
         method: "PATCH",
-        body: { count },
+        body: { cartLineItemId, count },
       }),
-      invalidatesTags: (result, error, arg) => {
-        return [{ type: "cart", id: "current" }];
-      },
     }),
   }),
 });
