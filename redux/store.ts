02deb2352b5e9f5d11490628e7b73fda84d145f1
5hyun <qhslsl@gmail.com>
2023-11-06 19:18:39 +0900

    Chore: createItemsSlice 리덕스 store에 추가

--- redux/store.ts
+++ redux/store.ts
@@ -3,12 +3,15 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { memberApi } from "@api/memberApi";
 import { myPageApi } from "@api/myPageApi";
 import { userInfoSlice } from "@redux/reducers/userInfoSlice";
+import createItems from "@components/SellerPages/CreateItems";
+import { createItemsSlice } from "@redux/reducers/createItemsSlice";
 
 export const store = configureStore({
   reducer: {
     [memberApi.reducerPath]: memberApi.reducer,
     [myPageApi.reducerPath]: myPageApi.reducer,
     [userInfoSlice.name]: userInfoSlice.reducer,
+    [createItemsSlice.name]: createItemsSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
