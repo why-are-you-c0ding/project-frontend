f5c55bce50d7288e86035ab0d450b14898b01df0
5hyun <qhslsl@gmail.com>
2023-11-07 21:41:40 +0900

    Feat: 상품 등록 Api 추가

---
+++ api/sellersApi.ts
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { ImageListType } from "react-images-uploading";
+import { Item } from "@typings/sellerPages";
+const URL = process.env.REACT_APP_BASE_URL!;
+const isDevelopment = process.env.REACT_START_MSW !== "true";
+
+export const sellersApi = createApi({
+  reducerPath: "sellersApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: isDevelopment ? URL : "/",
+    credentials: "include",
+  }),
+  tagTypes: ["sellers"],
+  endpoints: (builder) => ({
+    createImg: builder.mutation({
+      query: (image: File) => {
+        const formData = new FormData();
+        formData.append("images", image);
+
+        return {
+          url: "/images",
+          method: "POST",
+          body: formData,
+        };
+      },
+    }),
+    createItems: builder.mutation({
+      query: (data: Item) => {
+        return {
+          url: "/items",
+          method: "POST",
+          body: data,
+        };
+      },
+    }),
+  }),
+});
