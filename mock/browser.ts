8ec651a1e19efa76a8e1936227c1b64ccd5510a4
5hyun <qhslsl@gmail.com>
2023-12-24 01:11:16 +0900

    Feat: getItems mock 파일 추가

--- mock/browser.ts
+++ mock/browser.ts
@@ -2,5 +2,11 @@ import { setupWorker } from "msw";
 import { myPage } from "@mock/api/member/myPage";
 import { auth } from "@mock/api/member/auth";
 import { createItems } from "@mock/api/seller/createItems";
+import { getItems } from "@mock/api/items/getItems";
 
-export const worker = setupWorker(...auth, ...myPage, ...createItems);
+export const worker = setupWorker(
+  ...auth,
+  ...myPage,
+  ...createItems,
+  ...getItems,
+);
