153f2a62828b4cd45592f38dc615ee36ea477f68
5hyun <qhslsl@gmail.com>
2023-12-25 15:51:52 +0900

    Chore: query 제대로 받도록 수정

--- mock/api/items/getItems.ts
+++ mock/api/items/getItems.ts
@@ -1,10 +1,20 @@
 import { rest } from "msw";
-import { pagingAllItems } from "@mock/api/data/items/getItems";
+import {
+  createRandomItem,
+  pagingAllItems,
+} from "@mock/api/data/items/getItems";
+import { faker } from "@faker-js/faker";
 
 export const getItems = [
-  rest.get("/items?page=:id", async (req, res, ctx) => {
-    const { id } = req.params;
-    const items = { ...pagingAllItems, finalPage: !(+id === 10) };
+  rest.get("/items", async (req, res, ctx) => {
+    const id = req.url.searchParams.get("page")!;
+
+    const tempItems = faker.helpers.multiple(createRandomItem, { count: 10 });
+
+    const items = {
+      items: tempItems,
+      finalPage: !(+id === 10),
+    };
 
     return res(ctx.status(200), ctx.json(items));
   }),
