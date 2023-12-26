578696a6d31ea1bba0d3a056da9cf8c2d31fa70d
5hyun <qhslsl@gmail.com>
2023-12-26 17:15:28 +0900

    Feat: 제품 개별 타입 생성

--- typings/items.ts
+++ typings/items.ts
@@ -1,8 +1,33 @@
+// 전체 조회 시 사용하는 아이템 타입
 export interface item {
   itemId: number;
   itemName: string;
   shopName: string;
   basicPrice: number;
   imageUrl: string;
+  category: string;
+}
+
+export interface option {
+  optionId: number;
+  optionName: string;
+  price: number;
+}
+
+export interface optionGroup {
+  optionGroupId: number;
+  optionGroupName: string;
+  options: option[];
+}
+
+// 개별 조회 시 사용하는 아이템 타입
+export interface eachItem {
+  itemId: number;
+  itemName: string;
+  shopId: number;
+  shopName: string;
+  optionGroups: optionGroup[];
+  imageUrl: string;
+  information: string;
   category: string;
 }
