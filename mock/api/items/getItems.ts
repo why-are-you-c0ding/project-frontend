f0a2d2a456744a276a07af76af0204467ad05472
5hyun <qhslsl@gmail.com>
2023-12-26 12:57:54 +0900

    Fix: finalPage가 10이면 마지막 페이지

--- mock/api/items/getItems.ts
+++ mock/api/items/getItems.ts
@@ -1,19 +1,16 @@
 import { rest } from "msw";
-import {
-  createRandomItem,
-  pagingAllItems,
-} from "@mock/api/data/items/getItems";
+import { createRandomItem } from "@mock/api/data/items/getItems";
 import { faker } from "@faker-js/faker";
 
 export const getItems = [
   rest.get("/items", async (req, res, ctx) => {
     const id = req.url.searchParams.get("page")!;
 
-    const tempItems = faker.helpers.multiple(createRandomItem, { count: 10 });
+    const tempItems = faker.helpers.multiple(createRandomItem, { count: 20 });
 
     const items = {
       items: tempItems,
-      finalPage: !(+id === 10),
+      finalPage: +id === 10,
     };
 
     return res(ctx.status(200), ctx.json(items));
