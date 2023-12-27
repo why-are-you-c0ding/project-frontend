76f2a7d2c530d90aa9b3bfb7a3a361fb87e35c17
5hyun <qhslsl@gmail.com>
2023-12-27 16:57:21 +0900

    Revert "Chore: 에러 안나도록 수정"
    This reverts commit 3683adac4dcfb50c51bc9f5c3ad889cbf009715b.

--- components/Buy/index.tsx
+++ components/Buy/index.tsx
@@ -1,4 +1,12 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, {
+  ChangeEvent,
+  FormEvent,
+  FormEventHandler,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import StatusBar from "@components/UI/StatusBar";
 import {
   Wrapper,
   LeftSide,
@@ -17,109 +25,58 @@ import {
 } from "@components/Buy/styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
-import { useLocation } from "react-router-dom";
+import useSWR from "swr";
+import fetcher from "@utils/fetcher";
+import option from "@components/SellerPages/CreateItemsBodys/SellOption";
+import { Link, useLocation } from "react-router-dom";
+import { makeCartItems } from "@utils/makeCartItems";
+import axios from "axios";
+import { IEachData } from "@typings/db";
 import { itemsApi } from "@api/itemsApi";
-import { useAppSelector } from "@redux/hooks";
-import { toast } from "react-toastify";
-import { useNavigate } from "react-router";
-import { orderLineItem, orderOptionGroup } from "@typings/items";
+import { Select } from "@chakra-ui/react";
 
 const Buy = () => {
   const { pathname } = useLocation();
-  const isLogin = useAppSelector((state) => state.userInfo.isLogin);
   const [count, setCount] = useState(1);
-  const [selectOptions, setSelectOptions] = useState<{ [key: number]: number }>(
-    {},
-  );
+  const [eachPrices, setEachPrices] = useState<{ [key: number]: number }>({});
   const [price, setPrice] = useState(0);
   const { data, error, isFetching } = itemsApi.useGetEachItemsQuery(
     +pathname.slice(6),
   );
-  const navigate = useNavigate();
 
   const plusCount = () => {
     setCount((prev) => prev + 1);
   };
 
   const minusCount = () => {
-    if (count - 1 > 0) setCount((prev) => prev - 1);
+    setCount((prev) => prev - 1);
   };
 
   const onChangeSelect = useCallback(
     (optionGroupIdx: number, optionIdx: number) => {
       if (data) {
-        let tempSelectOption = { ...selectOptions };
-        let tempPrice = +data.price;
+        let temp = { ...eachPrices };
+        temp[optionGroupIdx] =
+          +data.optionGroups[optionGroupIdx].options[optionIdx].price;
 
-        tempSelectOption[optionGroupIdx] = optionIdx;
-        for (let i = 0; i < data.optionGroups.length; i++) {
-          tempPrice += +data.optionGroups[i].options[tempSelectOption[i]].price;
-        }
-
-        setSelectOptions(tempSelectOption);
-        setPrice(tempPrice);
+        setEachPrices(temp);
+        setPrice(Object.values(temp).reduce((a, c) => a + c, 0));
       }
     },
-    [data, selectOptions],
+    [data, eachPrices],
   );
-
-  // TODO: 장바구니 담기 버튼
-  const onClickCart = useCallback(() => {
-    if (!isLogin) {
-      toast.warning("로그인 해주세요.", {
-        position: toast.POSITION.TOP_CENTER,
-      });
-      return;
-    }
-  }, [isLogin]);
-
-  const onClickBuy = useCallback(() => {
-    if (!isLogin) {
-      toast.warning("로그인 해주세요.", {
-        position: toast.POSITION.TOP_CENTER,
-      });
-      return;
-    }
-
-    if (data) {
-      const orderOptionGroups: orderOptionGroup[] = [];
-
-      Object.values(selectOptions).map(
-        (selectIdx: number, optionIdx: number) => {
-          orderOptionGroups.push({
-            name: data.optionGroups[optionIdx].optionGroupName,
-            orderOption: {
-              name: data.optionGroups[optionIdx].options[selectIdx].optionName,
-              price: data.optionGroups[optionIdx].options[selectIdx].price,
-            },
-          });
-        },
-      );
-
-      const itemInfo: orderLineItem & { totalPayment: number } = {
-        itemId: data.itemId,
-        name: data.itemName,
-        count: count,
-        price: +data.price,
-        orderOptionGroups: orderOptionGroups,
-        totalPayment: price,
-      };
-
-      navigate("/checkout", { state: { itemInfo } });
-    }
-  }, [isLogin, data, count, price]);
 
   useEffect(() => {
     if (data) {
-      let tempSelectOption: { [key: number]: number } = {};
-      let tempPrice = +data.price;
+      let tempEachPrice: { [key: number]: number } = {};
+      let tempPrice = 0;
 
-      data.optionGroups.map((optionGroup, index) => {
-        tempSelectOption[index] = 0;
-        tempPrice += +optionGroup.options[0].price;
+      data.optionGroups.map((optionGroups, index) => {
+        tempEachPrice[index] = +optionGroups.options[0].price;
+        tempPrice += +optionGroups.options[0].price;
       });
 
-      setSelectOptions(tempSelectOption);
+      setEachPrices(tempEachPrice);
       setPrice(tempPrice);
     }
   }, [data]);
@@ -127,9 +84,7 @@ const Buy = () => {
   return (
     <div>
       {error && <div>새로고침하여 주세요.</div>}
-
       {isFetching && <div>로딩중...</div>}
-
       {!error && !isFetching && data && (
         <Wrapper>
           <LeftSide>
@@ -185,13 +140,25 @@ const Buy = () => {
                   <span>{count}</span>
                   <button onClick={plusCount}>+</button>
                 </CountBtn>
-                <SelectBtn onClick={onClickCart}>
+                <SelectBtn onClick={() => {}}>
                   <FontAwesomeIcon icon={faBasketShopping} />
                   <span>장바구니</span>
                 </SelectBtn>
               </div>
 
-              <BuyBtn onClick={onClickBuy}>구매</BuyBtn>
+              <Link
+                to={`/checkout/${location.pathname.split("/")[2]}`}
+                state={
+                  {
+                    // eachData: eachData,
+                    // optInfo: Data,
+                    // count: count,
+                    // total: total,
+                  }
+                }
+              >
+                <BuyBtn type="submit">구매</BuyBtn>
+              </Link>
             </Btn>
           </RightSide>
         </Wrapper>
