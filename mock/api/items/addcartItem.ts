f4fe237dd8539038f2fd3f71b264b93f7e684c71
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-12-28 00:56:04 +0900

    Feat: MSW 설정

---
+++ mock/api/items/addcartItem.ts
@@ -0,0 +1,9 @@
+import { rest } from "msw";
+
+export const addcartItem = [
+  rest.post("/carts/cart-line-items", async (req, res, ctx) => {
+    const { id } = req.params;
+
+    return res(ctx.status(200), ctx.json("s"));
+  }),
+];
