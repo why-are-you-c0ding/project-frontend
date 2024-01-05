a27fbc6a634d0dce0d498e6645b67232b2186319
5hyun <qhslsl@gmail.com>
2024-01-05 19:51:31 +0900

    Style: 개별 상품 조회 페이지 반응형 수정

--- pages/EachItem/styles.tsx
+++ pages/EachItem/styles.tsx
@@ -1,98 +1,75 @@
 import styled from "@emotion/styled";
 import { SignUpBtn } from "@pages/SignUp/styles";
 
-export const EachItemWrapper = styled.div`
-  display: block;
-  max-width: 1280px;
-  margin: 0 auto;
-  margin-top: 5rem;
-  margin-bottom: 5rem;
-`;
-
 export const Wrapper = styled.section`
-  display: grid;
-  grid-template-columns: 4fr 0.8fr 5fr;
-  padding-left: 3rem;
-  padding-right: 3rem;
-  padding-top: 1rem;
-
-  @media (max-width: 940px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-
-export const LeftSide = styled.section``;
-
-export const MiddleSide = styled.div`
-  border-left: 2px solid #c1b6b6;
-  margin-left: 50%;
-  height: 70vh;
-
-  @media (max-width: 940px) {
-    display: none;
-  }
-`;
-
-export const RightSide = styled.section`
-  padding: 1rem 1rem;
-  margin-left: 1rem;
-
-  @media (max-width: 940px) {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-
-    margin-left: 0;
-  }
-`;
-
-export const Item = styled.div`
-  margin-top: 2rem;
-  margin-right: 1rem;
+  width: 85vw;
+  height: 100%;
+  margin: 0 auto;
 
   display: flex;
-  justify-content: center;
-  align-content: center;
+  align-items: center;
+  gap: 2%;
+  padding-bottom: 5rem;
 
-  @media (max-width: 769px) {
-    margin-right: 0;
-  }
+  @media (max-width: 1000px) {
+    width: 100vw;
+    margin-top: 3rem;
 
-  & img {
-    width: 24rem;
-    height: 24rem;
-
-    @media (max-width: 769px) {
-      width: 20rem;
-      height: 20rem;
-
-      margin-top: -2rem;
-    }
+    flex-direction: column;
   }
 `;
 
-export const Itemdetail = styled.div`
-  & input {
-    margin-bottom: 1.5rem;
+export const ItemImgWrapper = styled.section`
+  width: 45%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > img {
+    width: 100%;
+    max-width: 29rem;
+    min-width: 20rem;
+  }
+
+  @media (max-width: 1000px) {
+    margin-bottom: 1rem;
+  }
+`;
+
+export const ItemInfoWrapper = styled.section`
+  width: 55%;
+
+  padding: 0 0.5rem;
+
+  & > div {
+    padding: 0 1rem;
+  }
+
+  @media (max-width: 1000px) {
+    width: 90%;
   }
 `;
 
 export const ItemName = styled.div`
-  font-size: 1.5rem;
+  font-size: 1.6rem;
+  font-weight: bold;
   margin-bottom: 0.5rem;
-  padding-bottom: 0.5rem;
-
   border-bottom: 1px solid rgba(0, 0, 0, 0.4);
 
-  @media (max-width: 940px) {
-    //margin-left: 3rem;
+  & > div {
+    margin-bottom: 0.5rem;
+  }
+
+  & > div > span:last-of-type {
+    margin-left: 0.1rem;
+
+    color: gray;
+    font-size: 1rem;
   }
 `;
 
 export const Option = styled.div`
-  margin-top: 1rem;
+  margin: 1rem 0;
 
   @media (max-width: 940px) {
     //margin-left: 3rem;
@@ -129,54 +106,30 @@ export const ItemInfo = styled.div`
   color: rgba(0, 0, 0, 0.4);
 `;
 
-export const TotalPrice = styled.div`
-  margin-top: 1rem;
-  font-size: 1.5rem;
-
-  margin-bottom: -2rem;
-
-  @media (max-width: 940px) {
-  }
-`;
-
-export const Btn = styled.section`
+export const ButtonWrapper = styled.section`
+  width: 100%;
   display: flex;
-  flex-direction: column;
+  justify-content: space-evenly;
   align-items: center;
+  gap: 2%;
 
-  width: 80%;
-  padding: 3rem 0;
-  margin-right: 7rem;
-
-  @media (max-width: 940px) {
-    //margin-left: 1rem;
-
-    margin-right: 0;
-
-    width: 100%;
-  }
-
-  & > div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  @media (max-width: 1000px) {
+    margin-bottom: 3rem;
   }
 `;
 
 export const BuyBtn = styled(SignUpBtn)`
-  width: 16rem;
+  width: 14rem;
   height: 4rem;
 
+  margin-right: 0;
+
   font-size: 1.5rem;
-  margin-top: 1rem;
 `;
 
 export const CountBtn = styled.div`
   display: flex;
   align-items: center;
-  margin-right: 1rem;
-
-  padding: 1rem 0;
 
   & > span {
     font-size: 1.2rem;
@@ -197,7 +150,7 @@ export const CountBtn = styled.div`
 `;
 
 export const SelectBtn = styled.button`
-  width: 16rem;
+  width: 14rem;
   height: 4rem;
   background-color: white;
   color: #696969;
@@ -215,5 +168,27 @@ export const SelectBtn = styled.button`
 
   &:hover {
     color: black;
+  }
+`;
+
+export const ItemDescription = styled.div`
+  font-size: 1.2rem;
+  margin-bottom: 1rem;
+`;
+
+export const ItemPrice = styled.div`
+  margin-top: 1rem;
+  font-size: 1.6rem;
+  margin-bottom: 2rem;
+`;
+
+export const ItemCount = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 1.6rem;
+  margin-bottom: 2rem;
+
+  & > span {
+    margin-right: 0.5rem;
   }
 `;
