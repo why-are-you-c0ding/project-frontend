eacf30cb634965bbd60f3892521899b2d658b4c1
5hyun <qhslsl@gmail.com>
2024-01-08 21:18:48 +0900

    Chore: 페이지네이션 테스트

--- components/SellerPages/RegisteredItems/index.tsx
+++ components/SellerPages/RegisteredItems/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Wrapper } from "@components/SellerPages/CreateItems/styles";
 import { TopHeader } from "@pages/MyPage/styles";
+import { sellersApi } from "@api/sellersApi";
+import { itemsApi } from "@api/itemsApi";
 
 export default function RegisteredItems() {
+  const { data } = sellersApi.useGetSellerItemsQuery(0);
+
+  console.log(data);
+
   return (
     <div>
       <Wrapper>
