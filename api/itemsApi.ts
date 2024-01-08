99759daf47008ff1432e8d25887d5a189fb53f90
5hyun <qhslsl@gmail.com>
2024-01-08 22:00:40 +0900

    Fix: 무한스크롤 값 저장 방식 공식 사이트 보고 수정

--- api/itemsApi.ts
+++ api/itemsApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { CartItemInfo } from "@typings/myPage";
 import { getCookie } from "@utils/cookie";
-import { addCartItem, eachItem, item } from "@typings/items";
+import { addCartItem, eachItem, item, ItemPaging } from "@typings/items";
 const URL = process.env.REACT_APP_BASE_URL;
 const isDevelopment = process.env.REACT_START_MSW !== "true";
 
@@ -16,19 +16,48 @@ export const itemsApi = createApi({
     getEachItems: builder.query<eachItem, number>({
       query: (id: number) => `/items/${id}`,
     }),
-    getAllItems: builder.query({
+    getAllItems: builder.query<ItemPaging, number>({
       query: (page: number) => {
         return {
           url: "/items/",
           params: { page },
         };
       },
+      serializeQueryArgs: ({ endpointName }) => {
+        return endpointName;
+      },
+      // Always merge incoming data to the cache entry
+      merge: (currentCache, newItems) => {
+        currentCache.finalPage = newItems.finalPage;
+        currentCache.items.push(...newItems.items);
+      },
+      // Refetch when the page arg changes
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
     }),
-    getSearchItems: builder.query({
+    getSearchItems: builder.query<
+      ItemPaging | { message: string },
+      { word: string; page: number }
+    >({
       query: ({ word, page }: { word: string; page: number }) => ({
         url: "/items/search",
         params: { keyword: word, page },
       }),
+      serializeQueryArgs: ({ queryArgs, endpointDefinition, endpointName }) => {
+        return endpointName + queryArgs.word;
+      },
+      // Always merge incoming data to the cache entry
+      merge: (currentCache, newItems) => {
+        if ("finalPage" in currentCache && "finalPage" in newItems) {
+          currentCache.finalPage = newItems.finalPage;
+          currentCache.items.push(...newItems.items);
+        }
+      },
+      // Refetch when the page arg changes
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
     }),
     addCartItem: builder.mutation({
       query: (cartItemInfo: addCartItem) => ({
