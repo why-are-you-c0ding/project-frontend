4ceff8d297ec0640310fa8ae060c329fdf260c98
5hyun <qhslsl@gmail.com>
2024-01-11 18:26:31 +0900

    Chore: 개별 상품 조회 데이터로 값 가져오기

--- components/SellerPages/DetailRegisteredItems/index.tsx
+++ components/SellerPages/DetailRegisteredItems/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Wrapper } from "@components/SellerPages/CreateItems/styles";
 import { TopHeader } from "@pages/MyPage/styles";
 import { useParams } from "react-router";
+import { itemsApi } from "@api/itemsApi";
 
 export default function DetailRegisteredItems() {
   const { id } = useParams();
+  const { data, error, isLoading } = itemsApi.useGetEachItemsQuery(id);
 
   return (
     <div>
