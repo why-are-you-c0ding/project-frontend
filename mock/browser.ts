80ff39c1f071fc31e4857006b5f3fb71d1074a38
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2024-01-16 15:27:39 +0900

    feat: 주문내역 연결

--- mock/browser.ts
+++ mock/browser.ts
@@ -5,12 +5,14 @@ import { createItems } from "@mock/api/seller/createItems";
 import { getItems } from "@mock/api/items/getItems";
 import { addcartItem } from "@mock/api/items/addcartItem";
 import { registeredItems } from "@mock/api/seller/RegisteredItems";
+import { orderHistoryItems } from "@mock/api/myPage/orderHistory";
 
 export const worker = setupWorker(
   ...auth,
   ...myPage,
   ...createItems,
   ...registeredItems,
+  ...orderHistoryItems,
   ...getItems,
   ...addcartItem,
 );
