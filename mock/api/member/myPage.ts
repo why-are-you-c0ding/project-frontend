c3021aefdc90e5372279e3f29720f08aa1abc3f0
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2024-01-16 15:28:23 +0900

    chore: 사용하지 않는 코드 삭제

--- mock/api/member/myPage.ts
+++ mock/api/member/myPage.ts
@@ -1,8 +1,6 @@
 import { rest } from "msw";
 import {
-  cartData,
   deleteCartItem,
-  orderData,
   updateCartItemCount,
 } from "@mock/api/data/member/myPage";
 import { getAllCartList } from "@mock/api/data/myPage/cartList";
@@ -18,18 +16,5 @@ export const myPage = [
   }),
   rest.patch("/carts/cart-line-items", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(updateCartItemCount));
-  }),
-  // 마이페이지-주문조회
-  rest.get("/order-line-items/customers", (req, res, ctx) => {
-    const lastLookUpOrderLineItemId = req.url.searchParams.get(
-      "lastLookUpOrderLineItemId",
-    );
-    return res(ctx.status(200), ctx.json(orderData));
-  }),
-  rest.get("/order-line-items/sellers", (req, res, ctx) => {
-    const lastLookUpOrderLineItemId = req.url.searchParams.get(
-      "lastLookUpOrderLineItemId",
-    );
-    return res(ctx.status(200), ctx.json(orderData));
   }),
 ];
