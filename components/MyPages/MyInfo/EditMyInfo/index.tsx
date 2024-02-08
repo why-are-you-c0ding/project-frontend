0e7516e7c98cecadafb9ef1c83f7b0000caf249f
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2024-02-08 16:11:17 +0900

    feat: 내 정보 변경 페이지 수정 중

--- components/MyPages/MyInfo/EditMyInfo/index.tsx
+++ components/MyPages/MyInfo/EditMyInfo/index.tsx
@@ -2,12 +2,48 @@ import React from "react";
 import ReponsiveBar from "@components/UI/ReponsiveBar";
 
 import { Wrapper } from "@components/MyPages/MyInfo/styles";
+import { TopHeader } from "@pages/MyPage/styles";
+import {
+  TableDetail,
+  TableSection,
+} from "@components/MyPages/MyInfo/EditMyInfo/styles";
+import { myPageApi } from "@api/myPageApi";
 
 const EditMyInfo = () => {
+  const { data: userData, isLoading } =
+    myPageApi.useGetUserDataQuery("getUserData");
+
   return (
     <div>
       <ReponsiveBar title={"내 정보 수정하기"} />
-      <Wrapper>내 정보를 수정해봅시다.</Wrapper>
+      {isLoading && <div>로딩중...</div>}
+      {userData && (
+        <Wrapper>
+          <TopHeader>내 정보 변경</TopHeader>
+          <TableSection>
+            <TableDetail>
+              <div>닉네임</div>
+              <div>{userData?.UserInfo.nickName}</div>
+            </TableDetail>
+            <TableDetail>
+              <div>아이디</div>
+              <div>{userData?.UserInfo.id}</div>
+            </TableDetail>
+            <TableDetail>
+              <div>이메일</div>
+              <div>{userData?.UserInfo.email}</div>
+            </TableDetail>
+            <TableDetail>
+              <div>나이</div>
+              <div>{userData?.UserInfo.age}</div>
+            </TableDetail>
+            <TableDetail>
+              <div>직위</div>
+              <div>{userData?.UserInfo.role}</div>
+            </TableDetail>
+          </TableSection>
+        </Wrapper>
+      )}
     </div>
   );
 };
--- components/MyPages/MyInfo/EditMyInfo/styles.tsx
+++ components/MyPages/MyInfo/EditMyInfo/styles.tsx
@@ -1,1 +1,23 @@
 import styled from "@emotion/styled";
+
+export const TableSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: fit-content;
+`;
+
+export const TableDetail = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  width: 100%;
+  background-color: gray;
+  border: 1px gray solid;
+  & :first-child {
+    background-color: #eef1f8;
+  }
+
+  & :last-child {
+    background-color: #ffffff;
+  }
+`;
