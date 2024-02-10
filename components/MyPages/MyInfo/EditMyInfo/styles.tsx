ebc8ba93b3e4dee05be1eccb72c00bb670ba7aaf
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2024-02-10 13:00:16 +0900

    style: 수정 페이지 스타일 수정

--- components/MyPages/MyInfo/EditMyInfo/styles.tsx
+++ components/MyPages/MyInfo/EditMyInfo/styles.tsx
@@ -9,17 +9,41 @@ export const TableSection = styled.section`
 
 export const TableDetail = styled.div`
   display: grid;
-  grid-template-columns: 1fr 5fr;
+  grid-template-columns: 8rem auto;
   width: 100%;
-  background-color: gray;
+  height: 4rem;
   border: 1px gray solid;
+
   & :first-child {
     background-color: #eef1f8;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 8rem;
   }
 
   & :last-child {
-    background-color: #ffffff;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 1rem;
+    & :first-child {
+      background-color: #ffffff;
+      padding: 0 1rem;
+      width: fit-content;
+    }
   }
 `;
 
-export const DetailEditBtn = styled.button``;
+export const DetailEditBtn = styled.button`
+  border: 1px #bcbfc6 solid;
+  color: #777;
+  background-color: #fafbf6;
+  border-radius: 5px;
+  min-width: 5rem;
+  padding: 5px 1px;
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
